94da95c6c87e6975a82829bfd93460a92de4d899
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 13:27:48 -0600

    Creation of the repository for users

---
+++ src/Repositories/Repository.Security/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Database.AlphaBank;
+
+namespace Repository.Security;
+
+public class UserRepository(AlphaBankDbContext context) {
+
+    private readonly AlphaBankDbContext _context = context;
+
+    
+
+}
