554481d857467dfb14972fa43b3ec7dbb164b53a
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 12:38:39 -0600

    The connection to the database has been configured.

--- src/Clients/WebClient/Program.cs
+++ src/Clients/WebClient/Program.cs
@@ -1,13 +1,19 @@
+using Database.AlphaBank;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Add database services
+builder.Services.AddDbContext<AlphaBankDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
