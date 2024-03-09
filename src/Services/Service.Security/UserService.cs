754ae15f55a81db94f6680bdad163c5159bee17e
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 19:07:03 -0600

    A library was created for the security service and the user service was created to validate passwords and user ids.

---
+++ src/Services/Service.Security/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Database.AlphaBank;
+
+namespace Service.Security;
+public class UserService(AlphaBankDbContext context)  {
+
+    private readonly AlphaBankDbContext _context = context;
+
+    public async Task<bool> ValidateUserId(int id) { 
+
+        var user = await _context.Users.FindAsync(id);
+
+        if(user == null) return false;
+
+        return (id == user.Employee.PersonId);
+    }
+
+    public async Task<bool> ValidateUserPassword(int id, string password) {
+
+        var user = await _context.Users.FindAsync(id);
+
+        if(user == null) return false;
+
+        return (user.Password.Equals(password));
+    }
+}
