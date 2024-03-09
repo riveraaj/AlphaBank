d6b2282e126764a00c9daff1c26765492da8f36b
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 22:52:33 -0600

    The DTO was created for a user doing the login process.

---
+++ src/Dtos/Dtos.AlphaBank/UserLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank {
+    internal class UserLoginDto {
+
+        [Required]
+        [Display(Name = "Usuario")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = null!;
+    }
+}
