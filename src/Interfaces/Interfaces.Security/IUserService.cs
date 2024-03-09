a1036a55a8a071b3fa06291418959470bf7a0773
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 19:09:07 -0600

    An interface was created for the user service

---
+++ src/Interfaces/Interfaces.Security/IUserService.cs
@@ -0,0 +1,6 @@
+﻿namespace Interfaces.Security;
+public  interface IUserService {
+
+    public Task<bool> ValidateUserId(int id);
+    public Task<bool> ValidateUserPassword(int id, string password);
+}
