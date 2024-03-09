c0fa4dfd740d8b3a1797e061d52dcce132d32002
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 19:11:13 -0600

    The IUserService is implemented

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -1,10 +1,12 @@
 ﻿using Database.AlphaBank;
+using Interfaces.Security;
 
 namespace Service.Security;
-public class UserService(AlphaBankDbContext context)  {
+public class UserService(AlphaBankDbContext context) : IUserService {
 
     private readonly AlphaBankDbContext _context = context;
 
+    //This function returns true or false if a user with the credential id is found.
     public async Task<bool> ValidateUserId(int id) { 
 
         var user = await _context.Users.FindAsync(id);
@@ -14,6 +16,7 @@ public class UserService(AlphaBankDbContext context)  {
         return (id == user.Employee.PersonId);
     }
 
+    //This function returns true or false if the contrasenna credentials are correct.
     public async Task<bool> ValidateUserPassword(int id, string password) {
 
         var user = await _context.Users.FindAsync(id);
