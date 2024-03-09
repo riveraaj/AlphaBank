055aec8936a89df45b13d826e038430ebc8001ff
riveraaj <jonathandavidr7@gmail.com>
2024-03-08 22:38:56 -0600

    Login method was created to validate all user data.

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -1,27 +1,38 @@
-﻿using Database.AlphaBank;
-using Interfaces.Security;
+﻿using Interfaces.Security;
 
 namespace Service.Security;
-public class UserService(AlphaBankDbContext context) : IUserService {
+public class UserService(IUserRepository userRepository) : IUserService {
 
-    private readonly AlphaBankDbContext _context = context;
+    private readonly IUserRepository _userRepository = userRepository;
+
+    //This method helps us to validate and send a message
+    //in response to the user's login process.
+    public async Task<string> Login() {
+
+        if (!await ValidateUserId(1))
+            return "Invalid identification";
+
+        if (!await ValidateUserPassword(1, "423rwcv"))
+            return "Wrong password";
+
+        return  "";
+    }
 
     //This function returns true or false if a user with the credential id is found.
     public async Task<bool> ValidateUserId(int id) { 
 
-        var user = await _context.Users.FindAsync(id);
+        var user = await _userRepository.GetByPersonIdAsync(id);
 
         if(user == null) return false;
-
-        return (id == user.Employee.PersonId);
+        else return true;
     }
 
     //This function returns true or false if the contrasenna credentials are correct.
     public async Task<bool> ValidateUserPassword(int id, string password) {
 
-        var user = await _context.Users.FindAsync(id);
+        var user = await _userRepository.GetByPersonIdAsync(id);
 
-        if(user == null) return false;
+        if (user == null) return false;
 
         return (user.Password.Equals(password));
     }
