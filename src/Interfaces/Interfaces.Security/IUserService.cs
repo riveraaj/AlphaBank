f6d8667dc1f1250c8780ed1b717593f0ff3a8c00
riveraaj <jonathandavidr7@gmail.com>
2024-03-09 12:18:47 -0600

    Added a method to implement

--- src/Interfaces/Interfaces.Security/IUserService.cs
+++ src/Interfaces/Interfaces.Security/IUserService.cs
@@ -1,6 +1,9 @@
-﻿namespace Interfaces.Security;
-public  interface IUserService {
+﻿using Dtos.AlphaBank;
 
-    public Task<bool> ValidateUserId(int id);
-    public Task<bool> ValidateUserPassword(int id, string password);
+namespace Interfaces.Security {
+    public interface IUserService {
+        public Task<(bool, UserAuthenticationDto?)> Login(UserLoginDto oUserLoginDto);
+        public Task<(bool, UserAuthenticationDto?)> ValidateUserId(int id);
+        public Task<bool> ValidateUserPassword(int id, string password);
+    }
 }
