4b17f0ffe728ba85851453cead1bc342977fe7a9
riveraaj <jonathandavidr7@gmail.com>
2024-03-09 12:52:44 -0600

    Added the method for login and its logic.

--- src/Clients/WebClient/Controllers/HomeController.cs
+++ src/Clients/WebClient/Controllers/HomeController.cs
@@ -1,18 +1,38 @@
+using Dtos.AlphaBank;
+using Interfaces.Security;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebClient.Models;
+using WebClient.Services;
 
 namespace WebClient.Controllers {
-    public class HomeController
-        (ILogger<HomeController> logger) : Controller {
+    internal class HomeController
+        (ILogger<HomeController> logger, IUserService oUserService) : Controller {
 
         private readonly ILogger<HomeController> _logger = logger;
+        private readonly IUserService _userService = oUserService;
 
         public IActionResult Index()
              => View();
 
         public IActionResult Privacy()
         => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Id", "Password")] UserLoginDto oUserLoginDto) {
+
+            var (result, userAuthentication) = await _userService.Login(oUserLoginDto);
+
+            if (!result) {
+                ViewData["Error"] = "*Hubo un error en el inicio de sesión, intentelo más tarde.";
+                return View();
+            }
+
+            await CookiesService.CreateAuthenticationCookies(HttpContext, userAuthentication!);
+
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
