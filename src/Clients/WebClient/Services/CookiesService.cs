fb630e3bee18854d33867c5d40cf860033c08472
riveraaj <jonathandavidr7@gmail.com>
2024-03-09 11:58:47 -0600

    A service was created to create user session cookies.

---
+++ src/Clients/WebClient/Services/CookiesService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace WebClient.Services {
+    partial class CookiesService {
+
+        //This method creates the claims and the cookie for a user session.
+        public static async Task CreateAuthenticationCookies(HttpContext oHttpContext)  {
+
+            var claims = new List<Claim> {
+                new (ClaimTypes.Role, "1"),
+                new (ClaimTypes.NameIdentifier, "1"),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await oHttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, 
+                                                        new ClaimsPrincipal(claimsIdentity));
+        }
+
+        //This method deletes cookies from the session.
+        public static async Task RemoveAuthenticationCookie(HttpContext oHttpContext) 
+            => await oHttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
