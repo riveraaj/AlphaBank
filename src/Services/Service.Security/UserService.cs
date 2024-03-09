c17ff862cb52d290514fc1e00297826032ff2e1a
riveraaj <jonathandavidr7@gmail.com>
2024-03-09 12:16:00 -0600

    Updated the Login and ValidateUserId methods and their returns.

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -2,35 +2,47 @@
 using Interfaces.Security;
 using Service.Security.Helpers;
 
-namespace Service.Security;
-public class UserService(IUserRepository userRepository) : IUserService {
+namespace Service.Security {
+    public class UserService(IUserRepository userRepository) : IUserService {
 
-    private readonly IUserRepository _userRepository = userRepository;
+        private readonly IUserRepository _userRepository = userRepository;
 
-    //This method helps us to validate and send a message
-    //in response to the user's login process.
-    public async Task<bool> Login(UserLoginDto oUserLoginDto)
-        => (!await ValidateUserId(oUserLoginDto.Id) 
-        && !await ValidateUserPassword(oUserLoginDto.Id, oUserLoginDto.Password));
+        //This method helps us to validate and send a message
+        //in response to the user's login process.
+        public async Task<(bool, UserAuthenticationDto?)> Login(UserLoginDto oUserLoginDto) {
 
-    //This function returns true or false if a user with the credential id is found.
-    public async Task<bool> ValidateUserId(int id) { 
+            //We obtain the values returned by the method
+            var (validatedUser, userAuth) = await ValidateUserId(oUserLoginDto.Id);
 
-        var user = await _userRepository.GetByPersonIdAsync(id);
+            var validatedUserPassword = await ValidateUserPassword(oUserLoginDto.Id,
+                                                                    oUserLoginDto.Password);
+            //Validate that the outputs are invalid
+            if (!validatedUser && !validatedUserPassword)
+                return (false, null);
 
-        if(user == null) return false;
-        else return true;
-    }
+            return (true, userAuth);
+        }
 
-    //This function returns true or false if the contrasenna credentials are correct.
-    public async Task<bool> ValidateUserPassword(int id, string password) {
+        //This function returns true or false if a user with the credential id is found.
+        public async Task<(bool, UserAuthenticationDto?)> ValidateUserId(int id) {
 
-        var user = await _userRepository.GetByPersonIdAsync(id);
+            var user = await _userRepository.GetByPersonIdAsync(id);
 
-        if (user == null) return false;
+            if (user == null) return (false, null);
+            else return (true, new UserAuthenticationDto
+            { Id = user.Id.ToString(), Role = user.Role.Description });
+        }
 
-        var pass = user.Password;
+        //This function returns true or false if the contrasenna credentials are correct.
+        public async Task<bool> ValidateUserPassword(int id, string password) {
 
-        return (pass.Equals(EncryptorHelper.ValidateEncryption(password, pass)));
+            var user = await _userRepository.GetByPersonIdAsync(id);
+
+            if (user == null) return false;
+
+            var pass = user.Password;
+
+            return (pass.Equals(EncryptorHelper.ValidateEncryption(password, pass)));
+        }
     }
 }
