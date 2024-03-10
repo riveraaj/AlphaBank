f73c54c2b044d6de6cb64595fd0313cce497eda7
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 16:35:39 -0600

    The Type Phone repository was created with a single method to obtain the cell phone types.

---
+++ src/Repositories/Repository.Common/TypePhoneRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Common {
+    public class TypePhoneRepository(AlphaBankDbContext context) {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<TypePhone>> GetAllAsync()
+            => await _context.TypePhones.ToListAsync();
+
+    }
+}
