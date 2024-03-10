afc63dc2285c987ebbe06a7e5eac1788bbc6650d
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 16:31:54 -0600

    Added references to other projects

---
+++ src/Repositories/Repository.Common/PhoneRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Common {
+    public class PhoneRepository(AlphaDbContext) {
+    }
+}
