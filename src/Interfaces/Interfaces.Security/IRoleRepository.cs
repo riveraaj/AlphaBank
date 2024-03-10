48d54bb060e768d295f0ff6c2208c6d4e46bb5af
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 17:04:32 -0600

    The interface for Role

---
+++ src/Interfaces/Interfaces.Security/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Security {
+    public interface IRoleRepository {
+
+        public Task<ICollection<Role>> GetAllAsync();
+
+        public Task CreateAsync(Role oRole);
+
+    }
+}
