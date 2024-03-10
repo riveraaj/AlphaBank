9bc2fd0d03f7ae6c29a4d807ed25b65bf6aa9f06
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 16:28:46 -0600

    The repository for the persons table was created with a single method of creating

---
+++ src/Repositories/Repository.Security/PersonRepository.cs
@@ -0,0 +1,12 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+
+namespace Repository.Security {
+    public class PersonRepository(AlphaBankDbContext context)  {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task Create(Person oPerson) 
+            => await _context.People.AddAsync(oPerson);
+    }
+}
