0fc67eac4a77da03efcd285c9407c1cd0d87ba73
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:25:45 -0600

    The repository was created for the client

---
+++ src/Repositories/Repository.BankAccounts/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+
+namespace Repository.BankAccounts {
+    public class CustomerRepository(AlphaBankDbContext context) {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task CreateAsync(Customer oCustomer)
+            => await _context.Customers.AddAsync(oCustomer);
+    }
+}
