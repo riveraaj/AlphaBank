183d0d534c1c4a2d68982f8471092e0c30d2619d
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:01:17 -0600

    MaritalStatus repository created

---
+++ src/Repositories/Repository.Common/MaritalStatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Common {
+    public class MaritalStatusRepository(AlphaBankDbContext context) : IMaritalStatusRepository  {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<MaritalStatus>> GetAllAsync() 
+            => await _context.MaritalStatuses.ToListAsync();
+    }
+}
