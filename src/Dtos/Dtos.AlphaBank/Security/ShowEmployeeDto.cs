cbf367d5a6e280af2af3f5fda15fa0199cbdf608
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 11:28:02 -0600

    The dto will be created in order to display the employee data

---
+++ src/Dtos/Dtos.AlphaBank/Security/ShowEmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Dtos.AlphaBank.Security {
+    public class ShowEmployeeDto {
+
+        public bool Status { get; set; }
+
+        public ShowPersonDto Person { get; set; } = null!;
+
+        public ShowUserDto User { get; set; } = null!;
+    }
+}
---
+++ src/Dtos/Dtos.AlphaBank/Security/ShowUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Dtos.AlphaBank.Security {
+    public class ShowUserDto {
+        public string EmailAddress { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
