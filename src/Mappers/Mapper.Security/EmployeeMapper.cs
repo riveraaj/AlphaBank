2d38687ffba627bc255f65586be7b3e0931442a1
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 21:34:00 -0600

    The mapper class for the employee table was created.

---
+++ src/Mappers/Mapper.Security/EmployeeMapper.cs
@@ -0,0 +1,27 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Security;
+
+namespace Mapper.Security {
+    public static class EmployeeMapper {
+
+        public static Person MapPerson(CreateEmployeeDto oCreateEmployeeDto)
+            => new() {
+                Id = (int) oCreateEmployeeDto.PersonId!,
+                Name = oCreateEmployeeDto.Name,
+                FirstName = oCreateEmployeeDto.FirstName,
+                SecondName = oCreateEmployeeDto.SecondName,
+                DateBirth = (DateOnly)oCreateEmployeeDto.DateBirth!,
+                Address = oCreateEmployeeDto.Address,
+                TypeIdentificationId = (byte) oCreateEmployeeDto.TypeIdentificationId!,
+                MaritalStatusId = (byte) oCreateEmployeeDto.MaritalStatusId!,
+                NationalityId = (byte) oCreateEmployeeDto.NationalityId!,
+            };
+
+        public static Employee MapEmployee(CreateEmployeeDto oCreateEmployeeDto)
+            => new() {
+                DateEntry = (DateOnly) oCreateEmployeeDto.DateEntry!,
+                SalaryCategoryId = (byte) oCreateEmployeeDto.SalaryCategoryId!
+            };
+
+    }
+}
