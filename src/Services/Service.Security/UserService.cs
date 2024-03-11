c252a90d0c4478bd1125acc58d3cfb0af49720ac
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 10:23:31 -0600

    The user service was created with a method to create a new user.

---
+++ src/Services/Service.Security/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Dtos.AlphaBank.Security;
+using Interfaces.Security;
+using Mapper.Security;
+using Service.Security.Helpers;
+
+namespace Service.Security {
+    public class UserService(IUserRepository userRepository) {
+
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public async Task<bool> Create(CreateUserDto oCreateUserDto) {
+
+            var user = UserMapper.MapUser(oCreateUserDto);
+
+            user.Password = EncryptorHelper.Encrypt(user.Password);
+
+            try {
+
+                await _userRepository.CreateAsync(user);
+
+                await _userRepository.SaveChangesAsync();
+
+                return true;
+
+            } catch (Exception) {
+                return false;
+                throw;
+            }
+        }
+    }
+}
