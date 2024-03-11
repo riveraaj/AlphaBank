9e10676b7545808b00a7c81b24ffc3af7e23cd52
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 22:41:10 -0600

    The UnitOfWork interface was created

---
+++ src/Interfaces/Interfaces.Security/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Interfaces.Security {
+    public interface IUnitOfWork {
+        public void BeginTransaction();
+        public void CommitTransaction();
+        public void Rollback();
+    }
+}
