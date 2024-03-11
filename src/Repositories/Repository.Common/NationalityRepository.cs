c71970e73e3aaad1ecd8439df48f4d66064c9376
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:07:11 -0600

    Nationality repository created

---
+++ src/Repositories/Repository.Common/NationalityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Common {
+    public class NationalityRepository(AlphaBankDbContext context) : INationalityRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Nationality>> GetAllAsync()
+            => await _context.Nationalities.ToListAsync();
+    }
+}
