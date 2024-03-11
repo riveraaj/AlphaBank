e9382cb5a3bff4a62fdcb81a8853d75a0ebd3d3c
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:12:02 -0600

    The repository was created for employee

---
+++ src/Repositories/Repository.Security/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Security;
+
+namespace Repository.Security {
+    public class EmployeeRepository(AlphaBankDbContext context) : IEmployeeRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task CreateAsync(Employee oEmployee)
+            => await _context.Employees.AddAsync(oEmployee);
+    }
+}
