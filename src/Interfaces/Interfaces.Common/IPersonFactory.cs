6957efeaafcb2a1ce2b78042af5a62f8a61cfe2c
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:32:52 -0600

    The interface for the people factory was created to create customers and employees.

---
+++ src/Interfaces/Interfaces.Common/IPersonFactory.cs
@@ -0,0 +1,17 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common {
+    public interface IPersonFactory {
+
+        public Person CreatePerson(string name, string firstName, 
+            string secondName, bool deceased, DateOnly dateBirth,
+            string address, byte typeIdentificationId, 
+            byte nationalityId, byte maritalStatusId);
+
+        public Employee CreateEmployee(bool status, DateOnly dateEntry, int personId, byte salaryCategoryId);
+
+        public Customer CreateCustomer(bool status, string emailAddress, decimal averageMonthlySalary, 
+            int personId, byte occupationId, byte customerStatusId);
+
+    }
+}
