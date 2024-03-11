20ef6a06173076cc4639c7ebbfee99c69c40e91c
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:30:06 -0600

    Its interface was implemented

--- src/Repositories/Repository.BankAccounts/CustomerRepository.cs
+++ src/Repositories/Repository.BankAccounts/CustomerRepository.cs
@@ -1,10 +1,15 @@
 ﻿using Data.AlphaBank;
 using Database.AlphaBank;
+using Interfaces.BankAccounts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository.BankAccounts {
-    public class CustomerRepository(AlphaBankDbContext context) {
+    public class CustomerRepository(AlphaBankDbContext context) : ICustomerRepository {
 
         private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Customer>> GetAllAsync()
+            => await _context.Customers.ToListAsync();
 
         public async Task CreateAsync(Customer oCustomer)
             => await _context.Customers.AddAsync(oCustomer);
