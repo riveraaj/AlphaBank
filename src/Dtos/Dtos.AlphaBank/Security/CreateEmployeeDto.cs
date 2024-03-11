c9e3ce5498d20417990367509c5043640aaf3182
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 21:33:10 -0600

    The dto was created to create an employee

--- src/Dtos/Dtos.AlphaBank/Security/CreateEmployeeDto.cs
+++ src/Dtos/Dtos.AlphaBank/Security/CreateEmployeeDto.cs
@@ -1,26 +1,39 @@
-﻿namespace Dtos.AlphaBank.Security {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Security {
     public class CreateEmployeeDto {
 
+        [Required]
         public int? PersonId { get; set; }
 
+        [Required]
         public string Name { get; set; } = null!;
 
+        [Required]
         public string FirstName { get; set; } = null!;
 
+        [Required]
         public string SecondName { get; set; } = null!;
 
+        [Required]
         public DateOnly? DateBirth { get; set; }
 
+        [Required]
         public string Address { get; set; } = null!;
 
+        [Required]
         public byte? TypeIdentificationId { get; set; }
 
+        [Required]
         public byte? NationalityId { get; set; }
 
+        [Required]
         public byte? MaritalStatusId { get; set; }
 
+        [Required]
         public DateOnly? DateEntry { get; set; }
 
+        [Required]
         public byte? SalaryCategoryId { get; set; }
 
     }
