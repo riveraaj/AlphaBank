6ab7681014d690d8bf0734c0bccb2716cf2b35bc
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:15:26 -0600

    The repository was created for the roles

---
+++ src/Repositories/Repository.Security/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Security;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Security {
+    public class RoleRepository(AlphaBankDbContext context) : IRoleRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task CreateAsync(Role oRole)
+            => await _context.Roles.AddAsync(oRole);
+
+        public async Task<ICollection<Role>> GetAllAsync()
+            => await _context.Roles.ToListAsync();
+    }
+}
