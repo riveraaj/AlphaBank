3e21e570a99b10e8071ee4e351df75ee406e35bd
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 10:18:54 -0600

    The mapper for the user table was created

---
+++ src/Mappers/Mapper.Security/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Security;
+
+namespace Mapper.Security {
+    public static class UserMapper {
+
+        public static User MapUser(CreateUserDto oCreateUserDto)
+            => new() {
+                EmailAddress = oCreateUserDto.Email,
+                Password = oCreateUserDto.Password,
+                RoleId = (byte) oCreateUserDto.RoleId!,
+                EmployeeId = (int) oCreateUserDto.EmployeeId!
+            };
+
+    }
+}
