b98736f71030b5c21cdf530514a00e68952d7148
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 11:28:30 -0600

    Added a function to bring employees

--- src/Interfaces/Interfaces.Security/IEmployeeRepository.cs
+++ src/Interfaces/Interfaces.Security/IEmployeeRepository.cs
@@ -3,6 +3,7 @@
 namespace Interfaces.Security {
     public interface IEmployeeRepository {
 
+        public Task<ICollection<Employee>> GetAllAsync();
         public Task CreateAsync(Employee oEmployee);
     }
 }
--- src/Repositories/Repository.Security/EmployeeRepository.cs
+++ src/Repositories/Repository.Security/EmployeeRepository.cs
@@ -1,13 +1,22 @@
 ﻿using Data.AlphaBank;
 using Database.AlphaBank;
 using Interfaces.Security;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository.Security {
     public class EmployeeRepository(AlphaBankDbContext context) : IEmployeeRepository {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task<ICollection<Employee>> GetAllAsync()
+              => await _context.Employees.Include(x => x.Person)
+                                       .Include(x => x.Person.Phones.FirstOrDefault())
+                                       .Include(x => x.Users.FirstOrDefault())
+                                       .Include(x => x.Users.FirstOrDefault()!.Role)
+                                       .ToListAsync();
+
         public async Task CreateAsync(Employee oEmployee)
             => await _context.Employees.AddAsync(oEmployee);
+
     }
 }
