42a98c550b3cc5264eac4a826bc77cb02c5fba6a
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 19:21:02 -0600

    Repository for SalaryCategory created

---
+++ src/Repositories/Repository.Security/SalaryCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Security;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Security {
+    public class SalaryCategoryRepository(AlphaBankDbContext context) : ISalaryCategoryRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<SalaryCategory>> GetAllAsync()
+            => await _context.SalaryCategories.ToListAsync();
+    }
+}
