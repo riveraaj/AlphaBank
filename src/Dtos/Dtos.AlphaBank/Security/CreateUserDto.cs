ab9b3398ad40707b1c45f0d2e4a03dbe581e3e61
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 10:16:02 -0600

    I created the dto to create a user

---
+++ src/Dtos/Dtos.AlphaBank/Security/CreateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Dtos.AlphaBank.Security {
+    public class CreateUserDto {
+
+        public string Email { get; set; } = null!; 
+        public string Password { get; set; } = null!;
+
+        public byte? RoleId { get; set; }
+
+        public int? EmployeeId { get; set; }
+
+    }
+}
