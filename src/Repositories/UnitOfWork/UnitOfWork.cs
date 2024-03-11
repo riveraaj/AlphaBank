8219d05f61d5a5d8319d4363866419179104ab77
riveraaj <jonathandavidr7@gmail.com>
2024-03-10 22:37:12 -0600

    A project was created in the UnitOfWork repository to handle the transactions.

---
+++ src/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Database.AlphaBank;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace UnitOfWork {
+    public class UnitOfWork (AlphaBankDbContext context) {
+
+        private readonly AlphaBankDbContext _context = context;
+        private IDbContextTransaction? _transaction;
+
+        //Init Transaction
+        public async void BeginTransaction()
+            => _transaction = await _context.Database.BeginTransactionAsync();
+
+        //Confirm Transaction
+        public async void CommitTransaction() {
+            try {
+               await _transaction?.CommitAsync()!;
+            }
+            catch(SqlException){
+                // Handling of exceptions when committing the transaction
+                Rollback();
+                throw;
+            }
+            finally {
+                _transaction?.Dispose();
+                _transaction = null;
+            }
+        }
+
+
+        // Revertir Transacción
+        public void Rollback() {
+            try {
+                _transaction?.Rollback();
+            }
+            finally {
+                _transaction?.Dispose();
+                _transaction = null;
+            }
+        }
+    }
+}
