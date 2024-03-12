98dc1c322a0c187dcc467281388a500b2f51f9df
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 21:08:15 -0600

    Data validations have been added.

--- src/Dtos/Dtos.AlphaBank/Security/UserAuthenticationDto.cs
+++ src/Dtos/Dtos.AlphaBank/Security/UserAuthenticationDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Dtos.AlphaBank.Security {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Security {
     public class UserAuthenticationDto {
+
+        [Required(ErrorMessage = "El campo id es obligatorio.")]
         public string Id { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo rol es obligatorio.")]
         public string Role { get; set; } = null!;
     }
 }
