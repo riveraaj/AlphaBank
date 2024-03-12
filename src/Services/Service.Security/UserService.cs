5b575b634d1d34a177a4b54c0bcc07b1df567d68
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 21:17:18 -0600

    The logger was implemented

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -1,12 +1,14 @@
 ﻿using Dtos.AlphaBank.Security;
 using Interfaces.Security;
 using Mapper.Security;
+using Microsoft.Extensions.Logging;
 using Service.Security.Helpers;
 
 namespace Service.Security {
-    public class UserService(IUserRepository userRepository) : IUserService {
+    public class UserService(IUserRepository userRepository, ILogger<UserService> logger) : IUserService {
 
         private readonly IUserRepository _userRepository = userRepository;
+        private readonly ILogger<UserService> _logger = logger;
 
         public async Task<bool> Create(CreateUserDto oCreateUserDto) {
 
@@ -17,16 +19,22 @@ namespace Service.Security {
             user.Password = EncryptorHelper.Encrypt(user.Password);
 
             try {
+                _logger.LogInformation("--- Start the creation of a user and save in database");
+
                 //Attempt to add the new user through the UserRepository and save changes asynchronously.
                 await _userRepository.CreateAsync(user);
 
                 await _userRepository.SaveChangesAsync();
 
+                _logger.LogInformation("--- I finish saving successfully");
+
                 //Return true to indicate successful creation.
                 return true;
 
-            } catch (Exception) {
+            } catch (Exception e) {
                 //If there's an exception during the process, return false.
+                _logger.LogInformation($"--- An error occurred while creating and saving to the database. More about error: {e.Message}");
+
                 return false;
             }
         }
