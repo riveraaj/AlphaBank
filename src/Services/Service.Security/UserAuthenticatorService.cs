56b41f1c3e344fa5ec4a89645ef2c6ff7bb3b800
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 19:38:11 -0600

    The code was refactored and the logs were implemented.

--- src/Services/Service.Security/UserAuthenticatorService.cs
+++ src/Services/Service.Security/UserAuthenticatorService.cs
@@ -1,16 +1,21 @@
 ﻿using Dtos.AlphaBank.Security;
 using Interfaces.Security;
+using Microsoft.Extensions.Logging;
 using Service.Security.Helpers;
 
-namespace Service.Security
-{
-    public class UserAuthenticatorService(IUserRepository userRepository) : IUserAuthenticatorService {
+namespace Service.Security {
+    public class UserAuthenticatorService(IUserRepository userRepository, 
+                                           ILogger<UserAuthenticatorService> logger) 
+                                           : IUserAuthenticatorService {
 
         private readonly IUserRepository _userRepository = userRepository;
+        private readonly ILogger<UserAuthenticatorService> _logger = logger;
 
         //This method helps us to validate and send a message
         //in response to the user's login process.
         public async Task<(bool, UserAuthenticationDto?)> UserAuthenticator(UserLoginDto oUserLoginDto) {
+
+            _logger.LogInformation("---- The user's credentials begin to be validated.");
 
             //We obtain the values returned by the method
             var (validatedUser, userAuth) = await ValidateUserId((int) oUserLoginDto.Id!);
@@ -18,8 +23,12 @@ namespace Service.Security
             var validatedUserPassword = await ValidateUserPassword((int) oUserLoginDto.Id,
                                                                     oUserLoginDto.Password);
             //Validate that the outputs are invalid
-            if (!validatedUser && !validatedUserPassword)
+            if (!validatedUser && !validatedUserPassword) {
+                _logger.LogError("---- User authentication failed");
                 return (false, null);
+            }
+
+            _logger.LogInformation("---- The user was successfully authenticated");
 
             return (true, userAuth);
         }
@@ -30,6 +39,7 @@ namespace Service.Security
             var user = await _userRepository.GetByPersonIdAsync(id);
 
             if (user == null) return (false, null);
+
             else return (true, new UserAuthenticationDto
             { Id = user.Id.ToString(), Role = user.RoleId.ToString() });
         }
