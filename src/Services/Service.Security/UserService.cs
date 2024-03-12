974f424e53efa447bcc8154a90de4ac4235f9b41
riveraaj <jonathandavidr7@gmail.com>
2024-03-11 21:14:17 -0600

    Refactoring

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -10,21 +10,24 @@ namespace Service.Security {
 
         public async Task<bool> Create(CreateUserDto oCreateUserDto) {
 
+            // Map CreateUserDto to a user object using some mapper (UserMapper).
             var user = UserMapper.MapUser(oCreateUserDto);
 
+            //Encrypt the user's password using some encryption method (EncryptorHelper).
             user.Password = EncryptorHelper.Encrypt(user.Password);
 
             try {
-
+                //Attempt to add the new user through the UserRepository and save changes asynchronously.
                 await _userRepository.CreateAsync(user);
 
                 await _userRepository.SaveChangesAsync();
 
+                //Return true to indicate successful creation.
                 return true;
 
             } catch (Exception) {
+                //If there's an exception during the process, return false.
                 return false;
-                throw;
             }
         }
     }
