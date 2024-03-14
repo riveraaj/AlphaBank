218385711db5f5124dac01e3e90dcd17999a6c01
riveraaj <jonathandavidr7@gmail.com>
2024-03-14 12:57:41 -0600

    The logic for mapping the entities was changed and the UnitOfWork was correctly implemented.

--- src/Services/Service.BankAccounts/CustomerServices.cs
+++ src/Services/Service.BankAccounts/CustomerServices.cs
@@ -3,6 +3,7 @@ using Dtos.AlphaBank.BankAccounts;
 using Interfaces.BankAccounts;
 using Interfaces.Security;
 using Mapper.BankAccounts;
+using Mapper.Common;
 using Microsoft.Extensions.Logging;
 
 namespace Service.BankAccounts
@@ -12,34 +13,39 @@ namespace Service.BankAccounts
                                  ICustomerRepository customerRepository,
                                  IOccupationRepository occupationRepository,
                                  ICustomerStatusRepository customerStatusRepository,
-                                 IPhoneRepository phoneRepository/*,
-                                 ILogger<CustomerService> logger*/) : ICustomerService
-                                 
-    {
+                                 IPhoneRepository phoneRepository,
+                                 ILogger<CustomerService> logger) : ICustomerService {
+
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
         private readonly IPersonRepository _personRepository = personRepository;
         private readonly ICustomerRepository _customerRepository = customerRepository;
         private readonly IOccupationRepository _occupationRepository = occupationRepository;
         private readonly ICustomerStatusRepository _customerstatusRepository = customerStatusRepository;
         private readonly IPhoneRepository _phoneRepository = phoneRepository;
-        //private readonly ILogger<CustomerService> _logger = logger;
+        private readonly ILogger<CustomerService> _logger = logger;
 
-        public async Task<bool> Create(CreateCustomerDto createCustomerDto)
-        {
-            // Map CreateCustomerDto to person, customer, and phone objects using CustomerMapper.
-            var person = CustomerMapper.MapPerson(createCustomerDto);
-            var phone = CustomerMapper.MapPhone(createCustomerDto);
-            var customer = CustomerMapper.MapCustomer(createCustomerDto); 
+        public async Task<bool> Create(CreateCustomerDto oCreateCustomerDto) {
+
+            //The id of the person is added to the reference of; phone
+            oCreateCustomerDto.Phone.PersonId = (int) oCreateCustomerDto
+                                                            .Person.PersonId!;
+
+            //Map CreateCustomerDto to person, customer, and phone objects using CustomerMapper.
+            var person = PersonMapper.MapPerson(oCreateCustomerDto.Person);
+            var phone = PhoneMapper.MapPhone(oCreateCustomerDto.Phone);
+            var customer = CustomerMapper.MapCustomer(oCreateCustomerDto); 
 
             // Set the status of the customer to true.
             customer.Status = true;
 
+            //Customer status value is set to regular
+            customer.CustomerStatusId = 1;
+
             // Set the deceased attribute of the person to false.
             person.Deceased = false;
 
-            try
-            {
-                //_logger.LogInformation("---- Start the transaction to create and save in the database an person, phone number and customer.");
+            try {
+                _logger.LogInformation("---- Start the transaction to create and save in the database an person, phone number and customer.");
 
                 //Begin a transaction using the unit of work.
                 await _unitOfWork.BeginTransaction();
@@ -51,52 +57,37 @@ namespace Service.BankAccounts
 
                 //Commit the transaction and save changes.
                 await _unitOfWork.CommitTransaction();
-                await _unitOfWork.SaveChangesAsync();
 
-                //_logger.LogInformation("---- Correctly completes the transaction.");
+                _logger.LogInformation("---- Correctly completes the transaction.");
 
                 // Return true to indicate successful creation.
                 return true;
-            }
-            catch (Exception e)
-            {
-                //_logger.LogError($"--- An error occurred while creating and saving to the database. More about error: {e.Message}");
+            } catch (Exception e) {
+                _logger.LogError($"--- An error occurred while creating and saving to the database. More about error: {e.Message}");
 
-                //If there's an exception during the process, rollback the transaction and return false.
-                await _unitOfWork.RollbackAsync();
+                //If there's an exception during the process, return false.
                 return false;
             }
         }
 
-        public async Task<List<Occupation>> GetAllOccupations()
-        {
-            try
-            {
+        public async Task<List<Occupation>> GetAllOccupations() {
+            try {
                 //Attempt to retrieve all occupations asynchronously from the OccupationRepository.
                 return (List<Occupation>)await _occupationRepository.GetAllAsync();
-
-            }
-            catch (Exception)
-            {
+            } catch (Exception) {
                 //If there's an exception during the process, return null.
                 return [];
             }
         }
 
-        public async Task<List<CustomerStatus>> GetAllCustomerStatuses()
-        {
-            try
-            {
+        public async Task<List<CustomerStatus>> GetAllCustomerStatuses() {
+            try {
                 //Attempt to retrieve all customer statuses asynchronously from the CustomerStatuseRepository.
                 return (List<CustomerStatus>)await _customerstatusRepository.GetAllAsync();
-
-            }
-            catch (Exception)
-            {
+            } catch (Exception) {
                 //If there's an exception during the process, return null.
                 return [];
             }
         }
-
     }
 }
