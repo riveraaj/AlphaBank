7048410247740908237e048ddaf28de509c11ed7
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-14 07:58:56 +0100

    Se creo una clase de repositorio de Occupations.

---
+++ src/Repositories/Repository.BankAccounts/OccupationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.BankAccounts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.BankAccounts {
+    public class OccupationRepository(AlphaBankDbContext context) : IOccupationRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Occupation>> GetAllAsync()
+          => await _context.Occupations.ToListAsync();
+    }
+}
