d4406317f0b506ff45878fb395d18950bfcc6eb9
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-14 07:54:52 +0100

    Se creo la interfaz para el repositorio de Occupation con un metodo.

---
+++ src/Interfaces/Interfaces.BankAccounts/IOccupationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.BankAccounts
+{
+    public interface IOccupationRepository  {
+
+        public Task<ICollection<Occupation>> GetAllAsync();
+
+    }
+}
