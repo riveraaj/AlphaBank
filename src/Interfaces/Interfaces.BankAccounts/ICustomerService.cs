2746de968403a9b0aa46f86d45a2c8f391ba279c
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-14 07:51:49 +0100

    Se creo la interfaz para el servicio de Customer con tres metodos.

---
+++ src/Interfaces/Interfaces.BankAccounts/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.BankAccounts;
+
+namespace Interfaces.BankAccounts
+{
+    public interface ICustomerService
+    {
+        public Task<bool> Create(CreateCustomerDto createCustomerDto);
+
+        public Task<List<Occupation>> GetAllOccupations();
+
+        public Task<List<CustomerStatus>> GetAllCustomerStatuses();
+    }
+}
