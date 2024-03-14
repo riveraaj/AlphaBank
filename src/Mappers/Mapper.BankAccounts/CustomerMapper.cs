88b968e27c6ecf98b8541a92e66f49fa4c8547be
riveraaj <jonathandavidr7@gmail.com>
2024-03-14 12:56:36 -0600

    Removed mapping methods that were created and abstracted in another project

--- src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
+++ src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
@@ -2,42 +2,15 @@
 using Data.AlphaBank;
 using Dtos.AlphaBank.BankAccounts;
 
-namespace Mapper.BankAccounts
-{
-    public static class CustomerMapper
-    {
+namespace Mapper.BankAccounts {
+    public static class CustomerMapper {
 
         public static Customer MapCustomer(CreateCustomerDto oCreateCustomerDto)
-            => new()
-            {
+            => new() {
                 EmailAddress = oCreateCustomerDto.EmailAddress, 
-                AverageMonthlySalary = (decimal) oCreateCustomerDto.AverageMonthlySalary,
-                PersonId = (int) oCreateCustomerDto.Id,
-                OccupationId = (byte) oCreateCustomerDto.OccupationId,
-                CustomerStatusId = (byte)oCreateCustomerDto.CustomerStatusId
+                AverageMonthlySalary = (decimal) oCreateCustomerDto.AverageMonthlySalary!,
+                PersonId = (int) oCreateCustomerDto.Person.PersonId!,
+                OccupationId = (byte) oCreateCustomerDto.OccupationId!
             };
-
-        public static Person MapPerson(CreateCustomerDto oCreateCustomerDto)
-            => new()
-            {
-                Id = (int) oCreateCustomerDto.Id,
-                Name = oCreateCustomerDto.Name,
-                FirstName = oCreateCustomerDto.FirstName,
-                SecondName = oCreateCustomerDto.SecondName,
-                DateBirth = (DateOnly) oCreateCustomerDto.DateBirth,
-                Address = oCreateCustomerDto.Address,
-                TypeIdentificationId = (byte)oCreateCustomerDto.TypeIdentificationId,
-                NationalityId = (byte)oCreateCustomerDto.NationalityId,
-                MaritalStatusId = (byte)oCreateCustomerDto.MaritalStatusId
-            };
-
-        public static Phone MapPhone(CreateCustomerDto oCreateCustomerDto)
-            => new()
-            {
-                Number = (int)oCreateCustomerDto.Number,
-                PersonId = (int)oCreateCustomerDto.Id,
-                TypePhoneId = (byte)oCreateCustomerDto.TypePhoneId
-            };
-
     }
 }
