d17a6aa541fc8b32faeec6b567c1bafc4b4f98a9
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-14 07:58:31 +0100

    Se creo una clase de repositorio de CustomerStatus.

---
+++ src/Repositories/Repository.BankAccounts/CustomerStatusRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.BankAccounts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.BankAccounts
+{
+    public class CustomerStatusRepository(AlphaBankDbContext context) : ICustomerStatusRepository
+    {
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<CustomerStatus>> GetAllAsync()
+          => await _context.CustomerStatuses.ToListAsync();
+    }
+}
