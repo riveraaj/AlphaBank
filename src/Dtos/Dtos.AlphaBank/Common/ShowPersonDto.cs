5e298cd788af2ed83438f7cc65ca1f54ddc58539
riveraaj <jonathandavidr7@gmail.com>
2024-03-13 20:27:49 -0600

    This dto has been moved from the security folder to common

---
+++ src/Dtos/Dtos.AlphaBank/Common/ShowPersonDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Dtos.AlphaBank.Common {
+    public class ShowPersonDto {
+
+        public string Name { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string SecondName { get; set; } = null!;
+
+        public int PhoneNumber { get; set; }
+    }
+}
