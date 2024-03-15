c30443229ce96a1815af392907dcc8085d5c752e
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 13:26:21 -0600

    Created the service class for CustomerStatus and implemented its interface and ICustomerStatusService methods.

---
+++ src/Services/Service.BankAccounts/CustomerStatusService.cs
@@ -0,0 +1,23 @@
+﻿using Data.AlphaBank;
+using Interfaces.BankAccounts;
+using System.Collections.Generic;
+
+namespace Service.BankAccounts {
+    public class CustomerStatusService(ICustomerStatusRepository customerStatusRepository)
+                                        : ICustomerStatusService {
+
+        private readonly ICustomerStatusRepository _customerStatusRepository
+            = customerStatusRepository;
+
+        public async Task<List<CustomerStatus>> GetAll() {
+            try {
+                //Retrieve all CustomerStatus asynchronously from the CustomerStatusRepository.
+                return (List<CustomerStatus>) await _customerStatusRepository.GetAllAsync();
+            }
+            catch {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+        }
+    }
+}
