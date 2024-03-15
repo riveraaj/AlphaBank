9020dc4f2b0468cd88208f825644566c695070ab
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 13:10:30 -0600

    Created the service class for Nationality and implemented its interface and INationalityService methods.

---
+++ src/Services/Service.Common/NationalityService.cs
@@ -0,0 +1,21 @@
+﻿using Data.AlphaBank;
+using Interfaces.Common;
+
+namespace Service.Common {
+    public class NationalityService(INationalityRepository nationalityRepository) 
+                                       : INationalityService {
+
+        private readonly INationalityRepository _nationalityRepository
+            = nationalityRepository;
+
+        public async Task<List<Nationality>> GetAll() {
+            try {
+                //Retrieve all Nationality asynchronously from the NationalityRepository.
+                return (List<Nationality>) await _nationalityRepository.GetAllAsync();
+            } catch {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+        }
+    }
+}
