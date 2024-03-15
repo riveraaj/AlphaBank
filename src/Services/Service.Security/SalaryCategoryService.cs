5f1fd1dddcc551ca5fbad6ec1ec9047c7dc884ba
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 13:20:06 -0600

    Created the service class for SalaryCategory and implemented its interface and ISalaryCategoryService methods.

---
+++ src/Services/Service.Security/SalaryCategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Data.AlphaBank;
+using Interfaces.Security;
+
+namespace Service.Security {
+    public class SalaryCategoryService(ISalaryCategoryRepository salaryCategoryRepository) 
+                                        : ISalaryCategoryService{
+
+        private readonly ISalaryCategoryRepository _salaryCategoryRepository
+            = salaryCategoryRepository;
+
+        public async Task<List<SalaryCategory>> GetAll() {
+            try {
+                //Attempt to retrieve all roles asynchronously from the RoleRepository.
+                return (List<SalaryCategory>)await _salaryCategoryRepository.GetAllAsync();
+            } catch {
+                //If there's an exception during the process, return null.
+                return [];
+            }
+        }
+    }
+}
