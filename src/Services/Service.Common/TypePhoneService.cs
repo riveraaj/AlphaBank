b3c85b4edbe2ee11c82be94927871e5c0129a7dd
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 13:15:02 -0600

    Created the service class for TypePhone and implemented its interface and ITypePhoneService methods.

---
+++ src/Services/Service.Common/TypePhoneService.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Interfaces.Common;
+using Interfaces.Security;
+
+namespace Service.Common {
+    public class TypePhoneService(ITypePhoneRepository typePhoneRepository) 
+                                    : ITypePhoneService {
+
+        private readonly ITypePhoneRepository _typePhoneRepository 
+            = typePhoneRepository;
+
+        public async Task<List<TypePhone>> GetAll()  {
+            try {
+                //Retrieve all TypePhone asynchronously from the TypePhoneRepository.
+                return (List<TypePhone>) await _typePhoneRepository.GetAllAsync();
+            } catch {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+        }
+    }
+}
