d3693b00128e25d63ec5e011376bc36cf2eb553e
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 00:44:43 -0600

    Added two methods: GetByPersonIdAsync and SaveChangesAsync.

--- src/Interfaces/Interfaces.BankAccounts/ICustomerRepository.cs
+++ src/Interfaces/Interfaces.BankAccounts/ICustomerRepository.cs
@@ -3,8 +3,12 @@
 namespace Interfaces.BankAccounts {
     public interface ICustomerRepository  {
 
+        public Task<Customer?> GetByPersonIdAsync(int id);
+
         public Task<ICollection<Customer>> GetAllAsync();
 
         public Task CreateAsync(Customer oCustomer);
+
+        public Task SaveChangesAsync();
     }
 }
