92289748440b16c029d599294339e775dd45e5b3
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 16:04:23 -0600

    Created the interface to create the repository class for the LoanApplication table

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ILoanApplication.cs
@@ -0,0 +1,12 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
+    public interface ILoanApplication {
+
+        public Task<ICollection<LoanApplication>> GetAllAsync();
+
+        public Task Create(LoanApplication oLoanApplication);
+
+        public Task UpdateApplicationStatus(int id);
+    }
+}
