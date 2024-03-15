735780c9ba13951755a49c6a52c7a859ef5d16c9
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 15:53:04 -0600

    The interface to create the repository class for the Deadline table was created.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IDeadlineRepository.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
+    public interface IDeadlineRepository {
+
+        public Task<ICollection<Deadline>> GetAllAsync();
+    }
+}
