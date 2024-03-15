7f0f91b0723e3a28a96d4e1ab5ca2c8202aca143
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 13:28:58 -0600

    Created the service class for Occupation and implemented its interface and IOccupationService methods.

---
+++ src/Services/Service.BankAccounts/OccupationService.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Interfaces.BankAccounts;
+
+namespace Service.BankAccounts {
+    public class OccupationService(IOccupationRepository occupationRepository)
+                                    : IOccupationService {
+
+        private readonly IOccupationRepository _occupationRepository
+            = occupationRepository;
+        
+        public async Task<List<Occupation>> GetAll() {
+            try {
+                //Retrieve all Occupation asynchronously from the OccupationRepository.
+                return (List<Occupation>) await _occupationRepository.GetAllAsync();
+            } catch {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+
+        }
+    }
+}
