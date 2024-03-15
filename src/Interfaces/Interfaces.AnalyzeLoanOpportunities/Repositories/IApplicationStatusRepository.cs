b69e3392007735a1c9674b5b2a0d2e5b801d9f75
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 15:56:11 -0600

    The interface to create the repository class for the ApplicationStatus table was created.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IApplicationStatusRepository.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
+    public interface IApplicationStatusRepository {
+
+        public Task<ICollection<ApplicationStatus>> GetAllAsync();
+    }
+}
