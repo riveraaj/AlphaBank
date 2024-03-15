2587eec5cb1d9f0abe262ceb7905792a79f480ce
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 15:54:51 -0600

    The interface to create the repository class for the TypeLoan table was created.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ITypeLoanRepository.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
+    public interface ITypeLoanRepository {
+
+        public Task<ICollection<TypeLoan>> GetAllAsync();
+    }
+}
