9c8faada0a128c955a4dde70777df795719bcd1f
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 00:45:00 -0600

    Two methods were eliminated

--- src/Interfaces/Interfaces.BankAccounts/ICustomerService.cs
+++ src/Interfaces/Interfaces.BankAccounts/ICustomerService.cs
@@ -1,12 +1,7 @@
-﻿using Data.AlphaBank;
-using Dtos.AlphaBank.BankAccounts;
+﻿using Dtos.AlphaBank.BankAccounts;
 
 namespace Interfaces.BankAccounts {
     public interface ICustomerService {
         public Task<bool> Create(CreateCustomerDto createCustomerDto);
-
-        public Task<List<Occupation>> GetAllOccupations();
-
-        public Task<List<CustomerStatus>> GetAllCustomerStatuses();
     }
 }
