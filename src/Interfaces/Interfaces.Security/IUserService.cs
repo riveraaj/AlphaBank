3a68235a5a6b8be8f202dc2f98a3138793d7c19c
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 00:16:18 -0600

    Added 1 new method: UserSetup

--- src/Interfaces/Interfaces.Security/IUserService.cs
+++ src/Interfaces/Interfaces.Security/IUserService.cs
@@ -3,6 +3,7 @@
 namespace Interfaces.Security {
     public  interface IUserService {
 
+        public Task<bool> UserSetup(CreateEmployeeDto oCreateEmployeeDto, IEmployeeRepository oEmployeeRepository);
         public Task<bool> Create(CreateUserDto oCreateUserDto);
     }
 }
