297a8e5e75a60788c8a51ac8becb4794daf433f5
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 16:24:59 -0600

    The repository class for the Interest table was created.

---
+++ src/Repositories/Repository.AnalyzeLoanOpportunities/InterestRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.AnalyzeLoanOpportunities.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.AnalyzeLoanOpportunities {
+    public class InterestRepository(AlphaBankDbContext context)
+                                    : IInterestRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Interest>> GetAllAsync()
+            => await _context.Interests.ToListAsync();
+    }
+}
