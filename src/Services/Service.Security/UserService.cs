a1630eec620e5c030a59ad6dff152b5c639c4f22
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 00:11:58 -0600

    We added a UserSetup method that was in the employee service but did not belong to it, that is why it was moved to this class.

--- src/Services/Service.Security/UserService.cs
+++ src/Services/Service.Security/UserService.cs
@@ -5,10 +5,30 @@ using Microsoft.Extensions.Logging;
 using Service.Security.Helpers;
 
 namespace Service.Security {
-    public class UserService(IUserRepository userRepository, ILogger<UserService> logger) : IUserService {
+    public class UserService(IUserRepository userRepository/*, ILogger<UserService> logger*/) : IUserService {
 
         private readonly IUserRepository _userRepository = userRepository;
-        private readonly ILogger<UserService> _logger = logger;
+        //private readonly ILogger<UserService> _logger = logger;
+
+        public async Task<bool> UserSetup(CreateEmployeeDto oCreateEmployeeDto, IEmployeeRepository oEmployeeRepository) {
+
+            //The id of the last registered employee is obtained to assign the user to the employee
+            var employee = await oEmployeeRepository.GetLastEmployeeAsync();
+            var employeeId = employee!.Id;
+
+            //The searched id is assigned to the model to perform the registration.
+            oCreateEmployeeDto.User.EmployeeId = employeeId;
+
+            //The user service is called to create the user.
+            var result = await Create(oCreateEmployeeDto.User);
+
+            if (!result) {
+                //If there's an exception during the process return false.
+                return false;
+            }
+            // Return true to indicate successful creation.
+            return true;
+        }
 
         public async Task<bool> Create(CreateUserDto oCreateUserDto) {
 
@@ -19,22 +39,20 @@ namespace Service.Security {
             user.Password = EncryptorHelper.Encrypt(user.Password);
 
             try {
-                _logger.LogInformation("--- Start the creation of a user and save in database");
+                //_logger.LogInformation("----- Create User: Start the creation of an user registry");
 
                 //Attempt to add the new user through the UserRepository and save changes asynchronously.
                 await _userRepository.CreateAsync(user);
-
                 await _userRepository.SaveChangesAsync();
 
-                _logger.LogInformation("--- I finish saving successfully");
+                //_logger.LogInformation("----- Create User: Creation completed and saved successfully.");
 
                 //Return true to indicate successful creation.
                 return true;
-
             } catch (Exception e) {
-                //If there's an exception during the process, return false.
-                _logger.LogError($"--- An error occurred while creating and saving to the database. More about error: {e.Message}");
+                //_logger.LogError($"----- Create User: An error occurred while creating and saving to the database. More about error: {e.Message}");
 
+                //If there's an exception during the process, return false.
                 return false;
             }
         }
