fb796221db0fb36cd1e44f6f7cb30cb7f3b0aadc
riveraaj <jonathandavidr7@gmail.com>
2024-03-15 00:16:57 -0600

    Added 3 new methods: GetByIdAsync, GetLastEmployeeAsync and RemoveAsync.

--- src/Repositories/Repository.Security/EmployeeRepository.cs
+++ src/Repositories/Repository.Security/EmployeeRepository.cs
@@ -8,6 +8,9 @@ namespace Repository.Security {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task<Employee?> GetByIdAsync(int id)
+            => await _context.Employees.FindAsync(id);
+
         public async Task<ICollection<Employee>> GetAllAsync()
               => await _context.Employees.Include(x => x.Person)
                                              .ThenInclude(p => p.Phones)
@@ -15,8 +18,24 @@ namespace Repository.Security {
                                              .ThenInclude(u => u.Role)
                                          .ToListAsync();
 
+        public async Task<Employee?> GetLastEmployeeAsync() {
+            var employeeList = await GetAllAsync();
+            return employeeList.Last();
+        }
+
         public async Task CreateAsync(Employee oEmployee)
             => await _context.Employees.AddAsync(oEmployee);
+
+        public async Task RemoveAsync(int id) {
+
+            //Search for the record in the table 
+            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee != null) _context.Employees.Remove(employee);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
 
     }
 }
