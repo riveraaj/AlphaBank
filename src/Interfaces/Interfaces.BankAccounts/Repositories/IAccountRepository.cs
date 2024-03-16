6311530b22b6ff7833a67520c3fae7f2d45c2f62
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-16 06:20:42 +0100

    Se creo la interfaz para el repositorio de Account con tres metodos.

---
+++ src/Interfaces/Interfaces.BankAccounts/Repositories/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.BankAccounts.Repositories
+{
+    public interface IAccountRepository
+    {
+
+        public Task<ICollection<Account>> GetAllAsync();
+
+        public Task CreateAsync(Account oAccount);
+
+        public Task SaveChangesAsync();
+
+    }
+}
