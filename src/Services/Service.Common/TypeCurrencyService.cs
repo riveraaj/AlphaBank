d7a1cfa4097fe2bf0d2aceffd2c9c1c63db463ee
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-16 06:28:00 +0100

    Se creo una clase de servicio de TypeCurrency.

---
+++ src/Services/Service.Common/TypeCurrencyService.cs
@@ -0,0 +1,29 @@
+﻿
+using Data.AlphaBank;
+using Interfaces.Common.Repositories;
+using Interfaces.Common.Services;
+
+namespace Service.Common
+{
+    public class TypeCurrencyService (ITypeCurrencyRepository typeCurrencyRepository)
+                                        : ITypeCurrencyService {
+
+        private readonly ITypeCurrencyRepository _typeCurrencyRepository
+            = typeCurrencyRepository;
+
+        public async Task<List<TypeCurrency>> GetAll()
+        {
+            try
+            {
+                //Retrieve all TypeCurrency asynchronously from the TypeCurrencyRepository.
+                return (List<TypeCurrency>) await _typeCurrencyRepository.GetAllAsync();
+            }
+            catch
+            {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+        }
+
+    }
+}
