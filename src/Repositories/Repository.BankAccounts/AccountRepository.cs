6795702b95e5e9a7c05f9bf6e1673afa9b45b080
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-16 06:25:55 +0100

    Se creo una clase de repositorio de Account.

---
+++ src/Repositories/Repository.BankAccounts/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.BankAccounts.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.BankAccounts
+{
+    public class AccountRepository (AlphaBankDbContext context) : IAccountRepository
+    {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Account>> GetAllAsync()
+            => await _context.Accounts.ToListAsync();
+
+        public async Task CreateAsync(Account oAccount)
+            => await _context.Accounts.AddAsync(oAccount);
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
