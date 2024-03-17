75a96a5b1264445857bdfffda71318fd2a7edce7
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-17 23:19:36 +0100

    Se agregaon 3 metodos a la interfaz del repositorio de Account.

--- src/Interfaces/Interfaces.BankAccounts/Repositories/IAccountRepository.cs
+++ src/Interfaces/Interfaces.BankAccounts/Repositories/IAccountRepository.cs
@@ -4,12 +4,17 @@ namespace Interfaces.BankAccounts.Repositories
 {
     public interface IAccountRepository
     {
-
         public Task<bool> CheckIfExistsByAccountNumberAsync(string accountNumber);
 
         public Task<ICollection<Account>> GetAllAsync();
 
+        public Task<Account> GetByAccountNumberAsync(string accountNumber);
+
+        public Task<ICollection<Account>> GetByCustomerIdAsync(int customerId);
+
         public Task CreateAsync(Account oAccount);
+
+        public Task RemoveAsync(string accountNumber);
 
         public Task SaveChangesAsync();
 
