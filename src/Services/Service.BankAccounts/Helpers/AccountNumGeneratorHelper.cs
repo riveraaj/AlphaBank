b0d9b6f845bf72f8f695dc1f801e95a68dec4395
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-17 06:07:48 +0100

    New Helper to get a new Accout Number

---
+++ src/Services/Service.BankAccounts/Helpers/AccountNumGeneratorHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Service.BankAccounts.Helpers
+{
+    internal class AccountNumGeneratorHelper
+    {
+
+        private static readonly Random rnd = new Random();
+
+        public static string AccountNumberGenerator(int typeAccountId, int typeCurrencyId)
+        {          
+            var countryCode = "CR";
+            var verificationDigit = "99";
+            var reservedCharacter = "0";
+            var bankCode = "000";
+            var branchCode = "000";
+            var accountType = typeAccountId.ToString().PadLeft(2, '0');
+            var currencyType = typeCurrencyId.ToString().PadLeft(2, '0');
+
+            var accountId = rnd.Next(1000000, 10000000);
+
+            var accountNumber = countryCode + verificationDigit + reservedCharacter + bankCode + branchCode + accountType + currencyType + accountId;
+
+            return accountNumber;
+        }
+
+    }
+}
