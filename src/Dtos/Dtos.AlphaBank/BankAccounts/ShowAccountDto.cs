c58c6420224e3b87f92c42a5a46f8607b5359688
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-17 23:16:44 +0100

    Se ceo el ShowAccountDto, esta pendiento definir lo que llevará a la vista.

---
+++ src/Dtos/Dtos.AlphaBank/BankAccounts/ShowAccountDto.cs
@@ -0,0 +1,15 @@
+﻿
+using Dtos.AlphaBank.Common;
+using Dtos.AlphaBank.Security;
+
+namespace Dtos.AlphaBank.BankAccounts
+{
+    public class ShowAccountDto
+    {
+        public string Id { get; set; } = null!;
+
+        public bool Status { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
