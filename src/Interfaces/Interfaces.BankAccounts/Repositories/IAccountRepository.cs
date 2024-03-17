8e05cd1e5d5866b9cf3eba69738d57104d326e8a
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-17 06:06:43 +0100

    Added the method CheckIfExistsByAccountNumberAsync to Repository Interface.

--- src/Interfaces/Interfaces.BankAccounts/Repositories/IAccountRepository.cs
+++ src/Interfaces/Interfaces.BankAccounts/Repositories/IAccountRepository.cs
@@ -5,6 +5,8 @@ namespace Interfaces.BankAccounts.Repositories
     public interface IAccountRepository
     {
 
+        public Task<bool> CheckIfExistsByAccountNumberAsync(string accountNumber);
+
         public Task<ICollection<Account>> GetAllAsync();
 
         public Task CreateAsync(Account oAccount);
