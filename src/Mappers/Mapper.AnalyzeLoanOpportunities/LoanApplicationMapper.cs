62d315b6e2fc1e3dda95dd53ecfde221a0cde0dd
riveraaj <jonathandavidr7@gmail.com>
2024-03-18 15:34:52 -0600

    A Mapper project was created for the AnalyzeLoanOpportunities module and a method was created to map a LoanApplication to a CreateLoanApplicationDto

---
+++ src/Mappers/Mapper.AnalyzeLoanOpportunities/LoanApplicationMapper.cs
@@ -0,0 +1,19 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace Mapper.AnalyzeLoanOpportunities {
+    public static class LoanApplicationMapper {
+
+        public static LoanApplication MapLoanApplication(CreateLoanApplicationDto oCreateLoanApplicationDto)
+            => new() {
+                AccountId = oCreateLoanApplicationDto.AccountId,
+                Amount = (decimal) oCreateLoanApplicationDto.Amount!,
+                DeadlineId = (byte) oCreateLoanApplicationDto.DeadlineId!,
+                InterestId = (byte) oCreateLoanApplicationDto.InterestId!,
+                Justification = oCreateLoanApplicationDto.Justification,
+                TypeCurrencyId = (byte) oCreateLoanApplicationDto.TypeCurrencyId!,
+                TypeLoanId = (byte) oCreateLoanApplicationDto.TypeLoanId!,
+                UserId = (int) oCreateLoanApplicationDto.UserId!
+            };
+    }
+}
