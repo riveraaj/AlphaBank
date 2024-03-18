22c47e83afc3b4118c6e0eb10d6113d41c9935c2
riveraaj <jonathandavidr7@gmail.com>
2024-03-18 16:04:28 -0600

    A new interface method has been implemented.

--- src/Repositories/Repository.BankAccounts/AccountRepository.cs
+++ src/Repositories/Repository.BankAccounts/AccountRepository.cs
@@ -3,12 +3,13 @@ using Database.AlphaBank;
 using Interfaces.BankAccounts.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace Repository.BankAccounts
-{
-    public class AccountRepository (AlphaBankDbContext context) : IAccountRepository
-    {
+namespace Repository.BankAccounts {
+    public class AccountRepository (AlphaBankDbContext context) : IAccountRepository {
 
         private readonly AlphaBankDbContext _context = context;
+
+        public async Task<Account?> GetByIdForLoanApplication(string id)
+            => await _context.Accounts.Include(x => x.Customer).FirstOrDefaultAsync(x => x.Id == id);                                    
 
         public async Task<bool> CheckIfExistsByAccountNumberAsync(string accountNumber)
             => await _context.Accounts.AnyAsync(x => x.Id.Equals(accountNumber));
@@ -21,22 +22,18 @@ namespace Repository.BankAccounts
         public async Task CreateAsync(Account oAccount)
             => await _context.Accounts.AddAsync(oAccount);
 
-        public async Task RemoveAsync(string accountNumber)
-        {
+        public async Task RemoveAsync(string accountNumber) {
             var account = await _context.Accounts.FirstOrDefaultAsync(x => x.Id == accountNumber);
 
-            if (account == null)
-                return;
+            if (account == null) return;
 
 
-            if (account.Balance != 0.0m)
-                return;
+            if (account.Balance != 0.0m) return;
 
             account.Status = false;
         }
 
         public async Task SaveChangesAsync()
             => await _context.SaveChangesAsync();
-
     }
 }
