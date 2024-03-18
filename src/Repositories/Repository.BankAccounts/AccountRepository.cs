88650bd050967aa48d214a72a179d113d1c22522
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-18 04:58:36 +0100

    Se eliminaron metodos innecesarios y se ajusto el metodo para obtener todas las cuentas en el Account Repository.

--- src/Repositories/Repository.BankAccounts/AccountRepository.cs
+++ src/Repositories/Repository.BankAccounts/AccountRepository.cs
@@ -14,15 +14,9 @@ namespace Repository.BankAccounts
             => await _context.Accounts.AnyAsync(x => x.Id.Equals(accountNumber));
 
         public async Task<ICollection<Account>> GetAllAsync()
-            => await _context.Accounts.ToListAsync();
-
-        public async Task<Account> GetByAccountNumberAsync(string accountNumber)
-            => await _context.Accounts.SingleAsync(x => x.Id == accountNumber);
-
-        public async Task<ICollection<Account>> GetByCustomerIdAsync(int customerId)
-            => await _context.Accounts
-                              .Where(x => x.CustomerId == customerId)
-                              .ToListAsync();
+            => await _context.Accounts.Include(x => x.TypeCurrency)
+                                        .Include(x => x.TypeAccount)
+                                        .ToListAsync();
 
         public async Task CreateAsync(Account oAccount)
             => await _context.Accounts.AddAsync(oAccount);
