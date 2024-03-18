9ba74500e286efb4eb8eec5b0a95e7511e0456f8
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-18 04:56:38 +0100

    Se añadio a la interfaz de servicio de Customer el metodo para obtener todos los clientes.

--- src/Interfaces/Interfaces.BankAccounts/Services/ICustomerService.cs
+++ src/Interfaces/Interfaces.BankAccounts/Services/ICustomerService.cs
@@ -5,5 +5,8 @@ namespace Interfaces.BankAccounts.Services
     public interface ICustomerService
     {
         public Task<bool> Create(CreateCustomerDto createCustomerDto);
+
+        public Task<List<ShowCustomerDto>> GetAll();
+
     }
 }
