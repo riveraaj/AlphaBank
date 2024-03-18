711fbc7317902240ebb583a8f9e7c90a68bff2d3
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-18 04:53:42 +0100

    Se creo un DTO para mostrar Clientes

---
+++ src/Dtos/Dtos.AlphaBank/BankAccounts/ShowCustomerDto.cs
@@ -0,0 +1,22 @@
+﻿
+using Dtos.AlphaBank.Common;
+
+namespace Dtos.AlphaBank.BankAccounts
+{
+    public class ShowCustomerDto
+    {
+
+        public bool Status { get; set; }
+
+        public string EmailAddress { get; set; } = null!;
+
+        public decimal AverageMonthlySalary { get; set; }
+
+        public string Occupation { get; set; } = null!;
+
+        public string CustomerStatus { get; set; } = null!;
+
+        public ShowPersonDto Person { get; set; } = null!;
+
+    }
+}
