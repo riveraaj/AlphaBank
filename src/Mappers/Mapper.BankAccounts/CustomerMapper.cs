561502beb80cfb35ee294c7c4d091429e6b20943
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-18 04:57:34 +0100

    Se agrego el metodo en CustomerMapper para mostrar los Clientes.

--- src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
+++ src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
@@ -1,6 +1,8 @@
 ﻿
 using Data.AlphaBank;
 using Dtos.AlphaBank.BankAccounts;
+using Dtos.AlphaBank.Common;
+using Dtos.AlphaBank.Security;
 
 namespace Mapper.BankAccounts {
     public static class CustomerMapper {
@@ -12,5 +14,28 @@ namespace Mapper.BankAccounts {
                 PersonId = (int) oCreateCustomerDto.Person.PersonId!,
                 OccupationId = (byte) oCreateCustomerDto.OccupationId!
             };
+
+        public static ShowCustomerDto MapShowCustomerDto(Customer oCustomer)
+        {
+            var showCustomerDto = new ShowCustomerDto
+            {
+                Status = oCustomer.Status,
+                EmailAddress = oCustomer.EmailAddress,
+                AverageMonthlySalary = oCustomer.AverageMonthlySalary,
+                Occupation = oCustomer.Occupation.Description,
+                CustomerStatus = oCustomer.CustomerStatus.Description,
+                Person = new ShowPersonDto
+                {
+                    Name = oCustomer.Person.Name,
+                    FirstName = oCustomer.Person.FirstName,
+                    SecondName = oCustomer.Person.SecondName
+                }
+            };
+
+            if (oCustomer.Person.Phones.Any())
+                showCustomerDto.Person.PhoneNumber = oCustomer.Person.Phones.First().Number;
+
+            return showCustomerDto;
+        }
     }
 }
