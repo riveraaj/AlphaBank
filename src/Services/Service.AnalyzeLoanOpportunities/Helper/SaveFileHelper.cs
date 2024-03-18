ace12ae77b8ae2e7b1d0e3a0f0196ea62b591c63
riveraaj <jonathandavidr7@gmail.com>
2024-03-18 16:26:07 -0600

    The .pdf extension has been implemented to the complete path.

--- src/Services/Service.AnalyzeLoanOpportunities/Helper/SaveFileHelper.cs
+++ src/Services/Service.AnalyzeLoanOpportunities/Helper/SaveFileHelper.cs
@@ -1,11 +1,11 @@
 ﻿namespace Service.AnalyzeLoanOpportunities.Helper {
     internal static class SaveFileHelper {
 
-        private const string filePath = @"C:\Documents\Proyecto-SoftwareIII";
+        private const string filePath = @"C:\Documents\Proyecto-SoftwareIII\Solicitud-Prestamo";
 
         public static string SaveFile(byte[] pdfContent, string pdfName) {
             //Merge the file name with the directory path to get the full path
-            string fullFilePath = Path.Combine(filePath, pdfName);
+            string fullFilePath = Path.Combine(filePath, pdfName, ".pdf");
 
             //Write the content of the PDF to the specified file
             File.WriteAllBytes(fullFilePath, pdfContent);
