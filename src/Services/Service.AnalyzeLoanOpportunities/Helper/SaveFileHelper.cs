32ca8d61f30fed720124e52cd2aab5a59818c66c
riveraaj <jonathandavidr7@gmail.com>
2024-03-18 16:06:40 -0600

    A class was created to store the files needed to create a loan application.

---
+++ src/Services/Service.AnalyzeLoanOpportunities/Helper/SaveFileHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Service.AnalyzeLoanOpportunities.Helper {
+    internal static class SaveFileHelper {
+
+        private const string filePath = @"C:\Documents\Proyecto-SoftwareIII";
+
+        public static string SaveFile(byte[] pdfContent, string pdfName) {
+            //Merge the file name with the directory path to get the full path
+            string fullFilePath = Path.Combine(filePath, pdfName);
+
+            //Write the content of the PDF to the specified file
+            File.WriteAllBytes(fullFilePath, pdfContent);
+
+            return fullFilePath;
+        }
+    }
+}
