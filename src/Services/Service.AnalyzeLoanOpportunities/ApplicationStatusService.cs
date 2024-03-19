8f1ab72cbbe3984c8675c369660f2daad46d0fc1
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:37:20 -0600

    The interface of this service and its methods were implemented.

---
+++ src/Services/Service.AnalyzeLoanOpportunities/ApplicationStatusService.cs
@@ -0,0 +1,21 @@
+﻿using Data.AlphaBank;
+using Interfaces.AnalyzeLoanOpportunities.Repositories;
+using Interfaces.AnalyzeLoanOpportunities.Services;
+
+namespace Service.AnalyzeLoanOpportunities {
+    public class ApplicationStatusService(IApplicationStatusRepository applicationStatusRepository) : IApplicationStatusService {
+
+        private readonly IApplicationStatusRepository _applicationStatusRepository
+            = applicationStatusRepository;
+
+        public async Task<List<ApplicationStatus>> GetAll() {
+            try {
+                //Attempt to retrieve all application status asynchronously from the RoleRepository.
+                return (List<ApplicationStatus>)await _applicationStatusRepository.GetAllAsync();
+            } catch {
+                //If there's an exception during the process, return null.
+                return [];
+            }
+        }
+    }
+}
