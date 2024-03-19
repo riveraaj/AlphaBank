9e5bfbd135d91d2788d95c0b4fb474fa60310cef
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:37:03 -0600

    An interface was created for the type loan service with a GetAll method.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ITypeLoanService.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Services {
+    public interface ITypeLoanService {
+
+        public Task<List<TypeLoan>> GetAll();
+    }
+}
