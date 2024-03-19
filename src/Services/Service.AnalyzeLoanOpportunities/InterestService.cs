7744faf0f8d0425a035a1168445dc974271ca766
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:37:24 -0600

    The interface of this service and its methods were implemented.

---
+++ src/Services/Service.AnalyzeLoanOpportunities/InterestService.cs
@@ -0,0 +1,20 @@
+﻿using Data.AlphaBank;
+using Interfaces.AnalyzeLoanOpportunities.Repositories;
+using Interfaces.AnalyzeLoanOpportunities.Services;
+
+namespace Service.AnalyzeLoanOpportunities {
+    public class InterestService(IInterestRepository interestRepository) : IInterestService {
+
+        private readonly IInterestRepository _interestRepository = interestRepository;
+
+        public async Task<List<Interest>> GetAll() {
+            try {
+                //Attempt to retrieve all interest asynchronously from the RoleRepository.
+                return (List<Interest>)await _interestRepository.GetAllAsync();
+            } catch {
+                //If there's an exception during the process, return null.
+                return [];
+            }
+        }
+    }
+}
