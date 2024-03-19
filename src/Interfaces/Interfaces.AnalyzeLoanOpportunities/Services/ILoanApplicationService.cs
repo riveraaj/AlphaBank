19c27101b28a9d5188267e911b3b6e378807e9f3
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:36:40 -0600

    An interface was created for the loan application service with two methods Create and ConvertToPdf.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ILoanApplicationService.cs
@@ -0,0 +1,10 @@
+﻿using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Services {
+    public interface ILoanApplicationService  {
+
+        public Task<bool> Create(CreateLoanApplicationDto oCreateLoanApplicationDto);
+
+        public byte[] ConvertToPdf(FileUploadDto fileUploadDto, string title);
+    }
+}
