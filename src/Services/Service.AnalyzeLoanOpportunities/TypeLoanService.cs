5f160016d9e15241a26b288551c89e72866837d4
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:37:30 -0600

    The interface of this service and its methods were implemented.

---
+++ src/Services/Service.AnalyzeLoanOpportunities/TypeLoanService.cs
@@ -0,0 +1,20 @@
+﻿using Data.AlphaBank;
+using Interfaces.AnalyzeLoanOpportunities.Repositories;
+using Interfaces.AnalyzeLoanOpportunities.Services;
+
+namespace Service.AnalyzeLoanOpportunities {
+    public class TypeLoanService(ITypeLoanRepository typeLoanRepository) : ITypeLoanService {
+
+        private readonly ITypeLoanRepository _typeLoanRepository = typeLoanRepository;
+
+        public async Task<List<TypeLoan>> GetAll() {
+            try  {
+                //Attempt to retrieve all typeLoan asynchronously from the RoleRepository.
+                return (List<TypeLoan>)await _typeLoanRepository.GetAllAsync();
+            } catch {
+                //If there's an exception during the process, return null.
+                return [];
+            }
+        }
+    }
+}
