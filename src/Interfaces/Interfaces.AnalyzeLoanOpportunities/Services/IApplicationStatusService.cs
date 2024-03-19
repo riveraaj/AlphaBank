fc5441387d0b7347890143a72ea2c9bed9124925
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:35:57 -0600

    An interface was created for the application status service with a GetAll method.

---
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/IApplicationStatusService.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.AnalyzeLoanOpportunities.Services {
+    public interface IApplicationStatusService {
+
+        public Task<List<ApplicationStatus>> GetAll();
+    }
+}
