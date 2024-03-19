78bfcbf6bb478de6667761aed2f2beec45eb7d50
riveraaj <jonathandavidr7@gmail.com>
2024-03-19 11:35:23 -0600

    A new method was created to map a LoanApplication to a ShowLoanApprovalHistoryDto

--- src/Mappers/Mapper.AnalyzeLoanOpportunities/LoanApplicationMapper.cs
+++ src/Mappers/Mapper.AnalyzeLoanOpportunities/LoanApplicationMapper.cs
@@ -1,5 +1,6 @@
 ﻿using Data.AlphaBank;
 using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+using Dtos.AlphaBank.Reports;
 
 namespace Mapper.AnalyzeLoanOpportunities {
     public static class LoanApplicationMapper {
@@ -14,6 +15,15 @@ namespace Mapper.AnalyzeLoanOpportunities {
                 TypeCurrencyId = (byte) oCreateLoanApplicationDto.TypeCurrencyId!,
                 TypeLoanId = (byte) oCreateLoanApplicationDto.TypeLoanId!,
                 UserId = (int) oCreateLoanApplicationDto.UserId!
+            };
+
+        public static ShowLoanApprovalHistoryDto MapShowLoanApprovalHistoryDto
+            (LoanApplication oLoanApplication) => new() {
+                Amount = oLoanApplication.Amount,
+                DateApplication = oLoanApplication.DateApplication,
+                Id = oLoanApplication.Id,
+                PersonId = oLoanApplication.Account.Customer.PersonId,
+                TypeLoan = oLoanApplication.TypeLoan.Description
             };
     }
 }
