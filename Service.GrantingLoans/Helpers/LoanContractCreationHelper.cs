1e2f7f749c2b4991e2b50be3231e663f8fba5336
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-24 04:53:47 +0100

    Se creo el Helper para crear el contrato para los prestamos

---
+++ Service.GrantingLoans/Helpers/LoanContractCreationHelper.cs
@@ -0,0 +1,182 @@
+﻿
+using Data.AlphaBank;
+using Humanizer;
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System.Globalization;
+
+using static System.Net.Mime.MediaTypeNames;
+using Text = iText.Layout.Element.Text;
+using Document = iText.Layout.Document;
+using iText.Bouncycastle.Crypto;
+
+namespace Service.GrantingLoans.Helpers
+{
+    internal static class LoanContractCreationHelper
+    {
+        private const string filePath = @"C:\Documents\Proyecto-SoftwareIII\Contratos";
+
+        public static string CreatePdf(LoanApplication loanApplication)
+        {
+
+            var spanish = new CultureInfo("es-CR");
+
+            string currency = "CRC";
+            string date = "20 de marzo del 2024";
+            string clientFullName = "Sergio Monge Cortés";
+            string identificationType = "cedula de identidad";
+            string identificationNumber = "1-1809-0090";
+            string address = "Mi casa";
+            decimal loanAmount = 15484090;
+            string interestPercentage = "10";
+            string loanTerm = "36";
+            decimal paymentAmount = (loanAmount / decimal.Parse(loanTerm)) + (loanAmount * (decimal.Parse(interestPercentage) / 100));
+            string description = "vacaciones";
+
+            string finalFilePath = System.IO.Path.Combine(filePath, loanApplication.Id + "_loan_contract_" + loanApplication.Account.CustomerId + ".pdf");
+
+
+            try
+            {
+                PdfWriter writer = new PdfWriter(finalFilePath);
+                PdfDocument pdf = new PdfDocument(writer);
+                PageSize pageSize = PageSize.LETTER; // Tamaño de página carta
+                pdf.SetDefaultPageSize(pageSize); // Establecer el tamaño de página
+
+                Document document = new Document(pdf);
+                document.SetMargins(65, 80, 65, 80); // Establecer los márgenes personalizados en 80 unidades = 1 pulgada
+
+                //Set Font to Arial (HELVETICA)
+                PdfFont font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+                PdfFont fontBold = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+
+                // Definir estilo para el título
+                Style titleStyle = new Style()
+                    .SetFont(fontBold)
+                    .SetFontSize(12)
+                    .SetTextAlignment(TextAlignment.CENTER)
+                    .SetMarginBottom(11 / 2);
+
+                // Definir estilo para el texto
+                Style paragraphStyle = new Style()
+                    .SetFont(font)
+                    .SetFontSize(11)
+                    .SetTextAlignment(TextAlignment.JUSTIFIED)
+                    .SetMarginBottom(11 / 2);
+
+                Text bank = new Text("Banco").SetFont(fontBold);
+                Text debtor = new Text("Deudor").SetFont(fontBold);
+                Text parts = new Text("Partes").SetFont(fontBold);
+                Text part = new Text("Parte").SetFont(fontBold);
+
+
+                document.Add(new Paragraph("Contrato de Otorgamiento de Préstamo Bancario").AddStyle(titleStyle));
+                document.Add(new Paragraph("\n"));
+                document.Add(new Paragraph(date).AddStyle(paragraphStyle).SetTextAlignment(TextAlignment.LEFT));
+                document.Add(new Paragraph("\n"));
+
+                Paragraph paragraph = new Paragraph();
+                paragraph.Add(new Text("ALPHA BANK").SetFont(fontBold));
+                paragraph.Add(", una institución bancaria costarricense registrada ante la Superintendencia General de Entidades" +
+                              " Financieras e inscrita en el Banco Central de Costa Rica identificada con la cedula jurídica 3-000-999999," +
+                              " con domicilio legal en San José, Escazú, San Rafael, Edificio Bancario y su Representante Legal es la ");
+                paragraph.Add(new Text("Sra. Ms. OLDA BUSTILLOS ORTEGA").SetFont(fontBold));
+                paragraph.Add(", portadora de la cédula de identidad 3-1111-1111; será llamada en el presente contrato como el “");
+                paragraph.Add(bank);
+                paragraph.Add("”.");
+                document.Add(paragraph.AddStyle(paragraphStyle));
+
+                paragraph = new Paragraph();
+                paragraph.Add(new Text(clientFullName.ToUpper()).SetFont(fontBold));
+                paragraph.Add(" de " + identificationType + " " + identificationNumber + " con domicilio en " + address + " en adelante denominado el “");
+                paragraph.Add(debtor);
+                paragraph.Add("”.");
+                document.Add(paragraph.AddStyle(paragraphStyle));
+
+                paragraph = new Paragraph();
+                paragraph.Add("El ");
+                paragraph.Add(bank);
+                paragraph.Add(" y el ");
+                paragraph.Add(debtor);
+                paragraph.Add(" se mencionarán conjuntamente como las “");
+                paragraph.Add(parts);
+                paragraph.Add("” y cada uno será una “");
+                paragraph.Add(part);
+                paragraph.Add("”.");
+                document.Add(paragraph.AddStyle(paragraphStyle));
+
+                document.Add(new Paragraph("Las Partes acuerdan el siguiente contrato de préstamo:").AddStyle(paragraphStyle));
+
+                document.Add(new Paragraph("1. Monto del Préstamo:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. El Banco otorga al Deudor un préstamo por un monto total de ₡" + ConvertirAMoneda(loanAmount.ToString(), currency) + " (" + long.Parse(loanAmount.ToString()).ToWords(spanish) + ").").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("2. Tasa de Interés:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. La tasa de interés acordada para este préstamo es del " + interestPercentage + "% anual.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("3. Plazo del Préstamo:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. El plazo de este préstamo es de " + loanTerm + " meses, comenzando a partir de la fecha de desembolso del préstamo.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("4. Forma de Pago:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. El Deudor deberá realizar pagos mensuales por el monto de " + ConvertirAMoneda(paymentAmount.ToString(), currency) + " en las fechas acordadas entre Las Partes.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("5. Penalizaciones por Pagos Atrasados:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. En caso de que el Deudor no realice los pagos en las fechas acordadas, se aplicará una penalización sobre el monto adeudado por cada día de retraso.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("6. Uso del Préstamo:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. El Deudor utilizará el monto del préstamo exclusivamente por motivo de " + description + ".").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("7. Vencimiento Anticipado:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. El Banco se reserva el derecho de dar por vencido anticipadamente el préstamo en caso de incumplimiento por parte del Deudor de cualquiera de las cláusulas de este contrato.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("8. Ley Aplicable:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. Este contrato se regirá e interpretará de acuerdo con las leyes de la República de Costa Rica.").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("9. Firma de las Partes:").AddStyle(paragraphStyle).SetMarginLeft(25));
+                document.Add(new Paragraph("i. Las partes reconocen haber leído, entendido y aceptado todas las cláusulas de este contrato y lo firman en señal de conformidad:").AddStyle(paragraphStyle).SetMarginLeft(50));
+
+                document.Add(new Paragraph("\n\nPor el Banco Alpha Bank").AddStyle(paragraphStyle));
+                document.Add(new Paragraph("\n____________________________").AddStyle(paragraphStyle));
+                document.Add(new Paragraph("Firma del Representante del Banco").AddStyle(paragraphStyle));
+
+                document.Add(new Paragraph("\n\n" + clientFullName).AddStyle(paragraphStyle));
+                document.Add(new Paragraph("\n____________________________").AddStyle(paragraphStyle));
+                document.Add(new Paragraph("Firma del Deudor").AddStyle(paragraphStyle));
+
+                document.Close();
+
+                return finalFilePath;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        static string ConvertirAMoneda(string cantidad, string codigoMoneda)
+        {
+            decimal cantidadDecimal = decimal.Parse(cantidad);
+            CultureInfo culture;
+
+            switch (codigoMoneda)
+            {
+                case "USD":
+                    culture = CultureInfo.GetCultureInfo("en-US");
+                    break;
+                case "CRC":
+                    culture = CultureInfo.GetCultureInfo("es-CR");
+                    break;
+                // Agregar más casos para otras monedas si es necesario
+                default:
+                    culture = CultureInfo.InvariantCulture;
+                    break;
+            }
+
+            return cantidadDecimal.ToString("C", culture).Replace(culture.NumberFormat.CurrencySymbol, "") + " " + codigoMoneda;
+        }
+    }
+}
