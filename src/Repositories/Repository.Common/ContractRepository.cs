d4c0c24d7931da2a4f349814812abd5959391dfd
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-25 06:02:05 +0100

    Se creo el repositorio de Contract con 3 metodos

---
+++ src/Repositories/Repository.Common/ContractRepository.cs
@@ -0,0 +1,24 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Common.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Common
+{
+    public class ContractRepository (AlphaBankDbContext context) : IContractRepository
+    {
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<Contract>> GetAllAsync()
+            => await _context.Contracts.Include(x => x.TypeContract)
+                                .ToListAsync();
+
+        public async Task CreateAsync(Contract oContract)
+            => await _context.Contracts.AddAsync(oContract);
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+
+    }
+}
