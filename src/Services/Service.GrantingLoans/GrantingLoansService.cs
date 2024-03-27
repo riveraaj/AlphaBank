4a82d2f151a6f5bb15b9d321f56bd41ec65156d3
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-27 06:52:16 +0100

    Se creo el servicio de GrantingLoans con 1 metodo.

---
+++ src/Services/Service.GrantingLoans/GrantingLoansService.cs
@@ -0,0 +1,23 @@
+﻿using Interfaces.GrantingLoans.Services;
+
+namespace Service.GrantingLoans
+{
+    public class GrantingLoansService : IGrantingLoansService
+    {
+
+        public async Task<bool> GrantingLoan(int idLoanApplication)
+        {
+			try
+			{
+
+				return true;
+			}
+			catch (Exception)
+			{
+
+				return false;
+			}
+        }
+
+    }
+}
