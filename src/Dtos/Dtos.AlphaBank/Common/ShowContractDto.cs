acd435b541793e33bb491daddda6120a4c53c170
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-29 23:54:08 +0100

    Se creo un DTO para mostrar los contratos

---
+++ src/Dtos/Dtos.AlphaBank/Common/ShowContractDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Dtos.AlphaBank.Common
+{
+    public class ShowContractDto
+    {
+        public string TypeContractDescription { get; set; } = null!;
+
+        public DateOnly DateStart { get; set; }
+
+        public DateOnly DateCompletion { get; set; }
+
+        public DateOnly DateUpdate { get; set; }
+
+        public string PathFile { get; set; } = null!;
+
+    }
+}
