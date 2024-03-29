25b5552d31487f1019dab711a6c935e3bb457cee
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 13:53:42 -0600

    A new DTO was created to display the client in LoanApplication.

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/ShowCustomerLoanDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Dtos.AlphaBank.AnalyzeLoanOpportunities {
+    public class ShowCustomerLoanDto {
+
+        public int UserId { get; set; }
+        public string FullName { get; set; } = null!;
+
+        public string MaritalStatus { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        public string Nationality { get; set; } = null!;
+
+        public string Occupation { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string AverageMonthlySalary { get; set; } = null!;
+    }
+}
