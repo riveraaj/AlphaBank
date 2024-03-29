637b91d2e4508c0987e362f8efebb416d3c42134
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:21:04 -0600

    ViewModel created for opening a bank account

---
+++ src/Clients/WebClient/Models/AccountOpeningViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Dtos.AlphaBank.BankAccounts;
+
+namespace WebClient.Models {
+    public class AccountOpeningViewModel {
+
+        public ShowCustomerDto ShowCustomerDto { get; set; } = null!;
+
+        public CreateAccountDto CreateAccountDto { get; set; } = null!;
+
+        public List<ShowAccountForPersonDto> ShowAccountForPersonDtoList { get; set; } = null!;
+    }
+}
