3791679bda9b8840a47a19523ff4da941fe104c3
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 13:55:17 -0600

    A new method has been added to the interface: GetById.

--- src/Interfaces/Interfaces.BankAccounts/Services/ICustomerService.cs
+++ src/Interfaces/Interfaces.BankAccounts/Services/ICustomerService.cs
@@ -1,12 +1,12 @@
-﻿using Dtos.AlphaBank.BankAccounts;
+﻿using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+using Dtos.AlphaBank.BankAccounts;
 
-namespace Interfaces.BankAccounts.Services
-{
-    public interface ICustomerService
-    {
+namespace Interfaces.BankAccounts.Services {
+    public interface ICustomerService {
         public Task<bool> Create(CreateCustomerDto createCustomerDto);
 
         public Task<List<ShowCustomerDto>> GetAll();
 
+        public Task<ShowCustomerLoanDto?> GetById(int id);
     }
 }
