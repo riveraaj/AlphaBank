1bfd78cb86332afb7af77ab46a4c775b375cbfde
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:23:44 -0600

    A common class was created to create selectLists that are used in different controllers.

---
+++ src/Clients/WebClient/Services/CommonService.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces.Common.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebClient.Services {
+    public class CommonService(ITypeCurrencyService typeCurrencyService) {
+
+        private readonly ITypeCurrencyService _typeCurrencyService = typeCurrencyService;
+
+        public async Task<IEnumerable<SelectListItem>> GetTypeCurrencySelectListItems() {
+
+            var typeCurrencyList = await _typeCurrencyService.GetAll();
+
+            return typeCurrencyList.Select(x => new SelectListItem {
+                Value = x.Id.ToString(),
+                Text = x.Description
+            });
+        }
+    }
+}
