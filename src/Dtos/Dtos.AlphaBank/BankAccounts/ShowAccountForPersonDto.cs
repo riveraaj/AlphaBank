d2b18dc328bc0be9343bee47493a7223d28422a9
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:27:11 -0600

    Created DTO to display an account for one person for the BankAccounts Module.

---
+++ src/Dtos/Dtos.AlphaBank/BankAccounts/ShowAccountForPersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Dtos.AlphaBank.BankAccounts {
+    public class ShowAccountForPersonDto {
+
+        public string AccountId { get; set; } = null!;
+
+        public string TypeCurrency { get; set; } = null!;
+
+        public string State { get; set; } = null!;
+    }
+}
