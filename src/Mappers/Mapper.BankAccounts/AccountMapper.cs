49964cea5ff6702930c2c22bccb242995f2f3c20
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:29:05 -0600

    Created MapShowAccountForPersonDto method

--- src/Mappers/Mapper.BankAccounts/AccountMapper.cs
+++ src/Mappers/Mapper.BankAccounts/AccountMapper.cs
@@ -1,22 +1,18 @@
 ﻿using Data.AlphaBank;
 using Dtos.AlphaBank.BankAccounts;
 
-namespace Mapper.BankAccounts
-{
-    public static class AccountMapper
-    {
+namespace Mapper.BankAccounts {
+    public static class AccountMapper {
 
         public static Account MapAccount(CreateAccountDto oCreateAccountDto)
-            => new()
-            {
+            => new() {
                 CustomerId = (int) oCreateAccountDto.CustomerId!,
                 TypeAccountId = (byte) oCreateAccountDto.TypeAccountId!,
                 TypeCurrencyId = (byte) oCreateAccountDto.TypeCurrencyId!,
             };
 
         public static ShowAccountDto MapShowAccountDto(Account oAccount)
-            => new()
-            {
+            => new() {
                 AccountNumber = oAccount.Id,
                 Balance = oAccount.Balance,
                 DateOpening = oAccount.DateOpening,
@@ -25,6 +21,11 @@ namespace Mapper.BankAccounts
                 CustomerID = oAccount.CustomerId
             };
 
-
+        public static ShowAccountForPersonDto MapShowAccountForPersonDto(Account oAccount)
+            => new() {
+                AccountId = oAccount.Id,
+                State = (oAccount.Status) ? "Activa" : "Inactiva",
+                TypeCurrency = oAccount.TypeCurrency.Description
+            };
     }
 }
