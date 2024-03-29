31327d529d001c3fd0443af819aaa6457d035ca6
riveraaj <jonathandavidr7@gmail.com>
2024-03-28 18:04:06 -0600

    a get method was created to get the info of a customer by identification

--- src/Clients/WebClient/Controllers/AnalyzeLoanOpportunitiesController.cs
+++ src/Clients/WebClient/Controllers/AnalyzeLoanOpportunitiesController.cs
@@ -1,7 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Interfaces.AnalyzeLoanOpportunities.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebClient.Controllers {
-    public class AnalyzeLoanOpportunitiesController : Controller {
+    public class AnalyzeLoanOpportunitiesController(ILoanApplicationService loanApplicationService) 
+                                                        : Controller {
+
+        private readonly ILoanApplicationService _loanApplicationService
+            = loanApplicationService;
+
         public IActionResult LoanApplication() => View();
+
+        public async Task<IActionResult> GetCustomer() {
+
+            //var customer = _loanApplicationService.
+
+            return View();
+        }
     }
 }
