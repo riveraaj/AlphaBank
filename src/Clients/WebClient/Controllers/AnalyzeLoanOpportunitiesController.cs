4b4493a817ccab3be646992646b4116287b4bf0c
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 13:48:26 -0600

    Updated the LoanApplication method logic to send and display all necessary data to the view and added service references: ILoanApplicationService, ICustomerService and AnalyzeLoanApplicationService.

--- src/Clients/WebClient/Controllers/AnalyzeLoanOpportunitiesController.cs
+++ src/Clients/WebClient/Controllers/AnalyzeLoanOpportunitiesController.cs
@@ -1,18 +1,56 @@
 ﻿using Interfaces.AnalyzeLoanOpportunities.Services;
+using Interfaces.BankAccounts.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebClient.Models;
+using WebClient.Services;
 
 namespace WebClient.Controllers {
-    public class AnalyzeLoanOpportunitiesController(ILoanApplicationService loanApplicationService) 
-                                                        : Controller {
+
+    [Authorize]
+    public class AnalyzeLoanOpportunitiesController(ILoanApplicationService loanApplicationService,
+                                                    ICustomerService customerService,
+                                                    AnalyzeLoanApplicationService analyzeLoanApplicationService) : Controller {
 
         private readonly ILoanApplicationService _loanApplicationService
             = loanApplicationService;
 
-        public IActionResult LoanApplication() => View();
+        private readonly ICustomerService _customerService = customerService;
 
-        public async Task<IActionResult> GetCustomer() {
+        private readonly AnalyzeLoanApplicationService _analyzeLoanApplicationService
+            = analyzeLoanApplicationService;
 
-            //var customer = _loanApplicationService.
+        [HttpGet]
+        public async Task<IActionResult> LoanApplication(int? id) {
+
+            //We obtain all the lists of data to be used in the select forms formatted in
+            //SelectList to enter them in a ViewData
+            var typeLoanList = await _analyzeLoanApplicationService.GetTypeLoanSelectListItems();
+            var deadlineList = await _analyzeLoanApplicationService.GetDeadlineSelectListItems();
+            var typeCurrencyList = await _analyzeLoanApplicationService.GetTypeCurrencySelectListItems();
+            var interestList = await _analyzeLoanApplicationService.GetInterestSelectListItems();
+
+            //ViewData is created for each SelectList and formatted as follows
+            ViewData["TypeLoan"] = new SelectList(typeLoanList, "Value", "Text");
+            ViewData["Deadline"] = new SelectList(deadlineList, "Value", "Text");
+            ViewData["TypeCurrency"] = new SelectList(typeCurrencyList, "Value", "Text");
+            ViewData["Interest"] = new SelectList(interestList, "Value", "Text");
+
+            if (id.HasValue) { //Validates if the parameter has data
+
+                //A customer is obtained from the id
+                var customer = await _customerService.GetById((int) id);
+
+                if (customer != null) {
+                    //The accounts related to the client are searched and saved in a
+                    //ViewData to be shown in a select of the form.
+                    var accountList = await _analyzeLoanApplicationService.GetAccountSelectListItems((int)id);
+                    ViewData["Account"] = new SelectList(accountList, "Value", "Text");
+
+                    return View(new LoanApplicationViewModel { ShowCustomerLoanDto = customer });
+                }
+            }
 
             return View();
         }
