3093fb807936d53adc4c1f49ba8fa8af789952c8
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:22:52 -0600

    A class was created to create a selectList that is used to create a bank account controllers.

---
+++ src/Clients/WebClient/Services/BankAccountService.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces.BankAccounts.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebClient.Services {
+    public class BankAccountService(ITypeAccountService typeAccountService) {
+
+        private readonly ITypeAccountService _typeAccountService1 = typeAccountService;
+
+        public async Task<IEnumerable<SelectListItem>> GetTypeAccountSelectListItems() {
+
+            var typeAccountList = await _typeAccountService1.GetAll();
+
+            return typeAccountList.Select(x => new SelectListItem {
+                Value = x.Id.ToString(),
+                Text = x.Description
+            });
+        }
+    }
+}
