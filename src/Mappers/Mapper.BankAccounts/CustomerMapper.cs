0072978fff0f84b2e48d9cb4e79eaa478163c785
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 17:29:30 -0600

    Changed the MapShowCustomerDto method to match the new DTO attributes.

--- src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
+++ src/Mappers/Mapper.BankAccounts/CustomerMapper.cs
@@ -2,7 +2,6 @@
 using Data.AlphaBank;
 using Dtos.AlphaBank.AnalyzeLoanOpportunities;
 using Dtos.AlphaBank.BankAccounts;
-using Dtos.AlphaBank.Common;
 using System.Globalization;
 
 namespace Mapper.BankAccounts {
@@ -30,26 +29,17 @@ namespace Mapper.BankAccounts {
             };
 
         public static ShowCustomerDto MapShowCustomerDto(Customer oCustomer)
-        {
-            var showCustomerDto = new ShowCustomerDto
-            {
-                Status = oCustomer.Status,
-                EmailAddress = oCustomer.EmailAddress,
-                AverageMonthlySalary = oCustomer.AverageMonthlySalary,
-                Occupation = oCustomer.Occupation.Description,
-                CustomerStatus = oCustomer.CustomerStatus.Description,
-                Person = new ShowPersonDto
-                {
-                    Name = oCustomer.Person.Name,
-                    FirstName = oCustomer.Person.FirstName,
-                    SecondName = oCustomer.Person.SecondName
-                }
-            };
-
-            if (oCustomer.Person.Phones.Any())
-                showCustomerDto.Person.PhoneNumber = oCustomer.Person.Phones.First().Number;
-
-            return showCustomerDto;
-        }
+         => new() {
+             CustomerId = oCustomer.Id.ToString(),
+             PersonId = oCustomer.PersonId,
+             Age = DateTime.Today.Year - oCustomer.Person.DateBirth.Year,
+             Address = oCustomer.Person.Address,
+             Email = oCustomer.EmailAddress,
+             FullName = $"{oCustomer.Person.Name} {oCustomer.Person.FirstName} {oCustomer.Person.SecondName}",
+             MaritalStatus = oCustomer.Person.MaritalStatus.Description,
+             Nationality = oCustomer.Person.Nationality.Description,
+             Occupation = oCustomer.Occupation.Description,
+             PhoneNumber = oCustomer.Person.Phones.FirstOrDefault()!.Number
+         };
     }
 }
