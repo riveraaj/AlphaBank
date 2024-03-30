ed63f3c758b0ba5443df128fc75f43d5966745dc
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 06:30:18 +0100

    Se crea la interfaz del repositorio de Loan con 7 metodos

---
+++ src/Interfaces/Interfaces.Common/Repositories/ILoanRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common.Repositories
+{
+    public interface ILoanRepository
+    {
+
+        public Task<Loan?> GetById(int id);
+
+        public Task<Loan?> GetByLoanApplicationId(int id);
+
+        public Task<ICollection<Loan>> GetByPersonId(int id);
+
+        public Task<ICollection<Loan>> GetAllAsync();
+
+        public Task CreateAsync(Loan oLoan);
+
+        public Task UpdateLoanStatus(int id, byte loanStatementId);
+
+        public Task SaveChangesAsync();
+
+    }
+}
