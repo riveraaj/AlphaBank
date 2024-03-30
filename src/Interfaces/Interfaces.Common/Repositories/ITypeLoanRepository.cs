0653d3441cfb3120152b20e0d6a30a971404fd77
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 03:18:17 +0100

    Se crearon las interfaces de TypeLoan Repository y Service con un metodo

---
+++ src/Interfaces/Interfaces.Common/Repositories/ITypeLoanRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common.Repositories
+{
+    public interface ITypeLoanRepository
+    {
+
+        public Task<ICollection<TypeLoan>> GetAllAsync();
+
+    }
+}
---
+++ src/Interfaces/Interfaces.Common/Services/ITypeLoanService.cs
@@ -0,0 +1,10 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common.Services
+{
+    public interface ITypeLoanService
+    {
+        public Task<List<TypeLoan>> GetAll();
+
+    }
+}
