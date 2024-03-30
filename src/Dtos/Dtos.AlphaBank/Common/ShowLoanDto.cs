00603ff0830aeabab9e4707109c81181c750d4af
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 22:17:06 +0100

    Se creo el Loan DTO BASICO con sus 4 atributos.

---
+++ src/Dtos/Dtos.AlphaBank/Common/ShowLoanDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Dtos.AlphaBank.Common
+{
+    public class ShowLoanDTO
+    {
+        public int Id { get; set; }
+        public int RemainingQuotas { get; set; }
+        public int LoanApplicationId { get; set; }
+        public string LoanStatementDescription { get; set; } = null!;
+
+
+    }
+}
