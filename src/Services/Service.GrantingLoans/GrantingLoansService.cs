7e4b1922f93f9b6b55a84eb6762b2e45db1fd9d4
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 06:32:02 +0100

    Se añadio al otorgamiento de prestamos la creacion en la tabla Loan

--- src/Services/Service.GrantingLoans/GrantingLoansService.cs
+++ src/Services/Service.GrantingLoans/GrantingLoansService.cs
@@ -7,10 +7,12 @@ namespace Service.GrantingLoans
 {
     public class GrantingLoansService (ILoanApplicationRepository loanApplicationRepository,
                                        IContractService contractService,
+                                       ILoanService loanService,
                                        ILogger<GrantingLoansService> logger) : IGrantingLoansService {
 
         private readonly ILoanApplicationRepository _loanApplicationRepository = loanApplicationRepository;
         private readonly IContractService _contractService = contractService;
+        private readonly ILoanService _loanService = loanService;
         private readonly ILogger<GrantingLoansService> _logger = logger;
 
         public async Task<bool> GrantingLoan(int idLoanApplication) {
@@ -25,9 +27,9 @@ namespace Service.GrantingLoans
 
                 _logger.LogInformation("----- Loan Granting: Create a registry of Contract.");
                 //Create the Contract PDF File and the Contract in the DataBase
-                var result = await _contractService.LoanTypeCreate(oLoanApplication);
+                var contractCreation = await _contractService.LoanTypeCreate(oLoanApplication);
                 // Check if we Succesfully create the Contract PDF File and the Contract in the DataBase
-                if (!result) return false;
+                if (!contractCreation) return false;
 
                 _logger.LogInformation("----- Loan Granting: Update the ApplicationStatus of the LoanApplication.");
                 //Update the LoanApplication Status to 2 or "Approbado"
@@ -36,9 +38,9 @@ namespace Service.GrantingLoans
 
                 _logger.LogInformation("----- Loan Granting: Create the Loan in the data base.");
                 //Create Loan Record in the Data Base
-                /* AQUI VA EL INSERT EN LA TABLA LOAN */
+                var loanCreation = await _loanService.Create(oLoanApplication);
                 //Check if we Succesfully create Loan Record in the Data Base
-                /* AQUI REVISO EL INSERT EN LA TABLA LOAN */
+                if (!loanCreation) return false;
 
                 _logger.LogInformation("----- Loan Granting: Loan Granting process completed successfully.");
 
