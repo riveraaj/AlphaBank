e9828d0a91fae974b69ceb339f5ed46a74e297c3
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 06:33:23 +0100

    Se crea el servicio de Loan con 1 metodo

---
+++ src/Services/Service.Common/LoanStatementService.cs
@@ -0,0 +1,27 @@
+﻿using Data.AlphaBank;
+using Interfaces.Common.Repositories;
+using Interfaces.Common.Services;
+
+namespace Service.Common
+{
+    public class LoanStatementService (ILoanStatementRepository loanStatementRepository)
+                                        : ILoanStatementService
+    {
+        private readonly ILoanStatementRepository _loanStatementRepository
+            = loanStatementRepository;
+
+        public async Task<List<LoanStatement>> GetAll()
+        {
+            try
+            {
+                //Retrieve all LoanStatement asynchronously from the LoanStatementRepository.
+                return (List<LoanStatement>) await _loanStatementRepository.GetAllAsync();
+            }
+            catch
+            {
+                // If there's an exception during the process, return an empty list.
+                return [];
+            }
+        }
+    }
+}
