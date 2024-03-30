7edd4e7c000138e8193786ac931a48437fa816ab
riveraaj <jonathandavidr7@gmail.com>
2024-03-29 21:38:10 -0600

    Renamed AccountViewModel to AccountOppeningViewModel

--- src/Clients/WebClient/Models/AccountOppeningViewModel.cs
+++ src/Clients/WebClient/Models/AccountOppeningViewModel.cs
@@ -1,12 +1,10 @@
 ﻿using Dtos.AlphaBank.BankAccounts;
 
 namespace WebClient.Models {
-    public class AccountViewModel {
+    public class AccountOppeningViewModel {
 
         public ShowCustomerDTO Customer { get; set; } = null!;
-
         public CreateAccountDTO Account { get; set; } = null!;
-
         public List<ShowAccountForPersonDTO> AccountList { get; set; } = null!;
     }
 }
