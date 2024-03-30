656bbc1aefb3fe1cce0c3a17f02d63071f43da99
riveraaj <jonathandavidr7@gmail.com>
2024-03-30 13:12:23 -0600

    Changed the driver routing pattern

--- src/Clients/WebClient/Program.cs
+++ src/Clients/WebClient/Program.cs
@@ -92,6 +92,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}",
+    defaults: new { controller = "Home", action = "Index" });
 
 app.Run();
