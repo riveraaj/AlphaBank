f73faa14b8f01ada67ad1dc7f27695692430281b
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-30 22:17:18 +0100

    Se creo el Loan MAPPER DTO BASICO con sus 4 atributos.

---
+++ src/Mappers/Mapper.Common/LoanMapper.cs
@@ -0,0 +1,19 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Common;
+
+namespace Mapper.Common
+{
+    public static class LoanMapper
+    {
+
+        public static ShowLoanDTO MapShowLoanDTO(Loan oLoan)
+            => new()
+            {
+                Id = oLoan.Id,
+                RemainingQuotas = oLoan.RemainingQuotas,
+                LoanApplicationId = oLoan.LoanApplicationId,
+                LoanStatementDescription = oLoan.LoanStatement.Description
+            };
+
+    }
+}
