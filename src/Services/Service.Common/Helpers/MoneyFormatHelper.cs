d74315772402e8bc7079e8fa77351a77f744a0c8
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-31 07:51:32 +0200

    Helper para darle formato de dinero a los montos

---
+++ src/Services/Service.Common/Helpers/MoneyFormatHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Service.Common.Helpers
+{
+    public static class MoneyFormatHelper
+    {
+
+        // This method is used to change amounts to money format depending of the currency
+        public static string MoneyFormat(string amount, string currecncy)
+        {
+            decimal cantidadDecimal = decimal.Parse(amount);
+
+            CultureInfo culture = currecncy switch
+            {
+                "USD" => CultureInfo.GetCultureInfo("en-US"),
+                "CRC" => CultureInfo.GetCultureInfo("es-CR"),
+                "EUR" => CultureInfo.GetCultureInfo("es-ES"),
+                _ => CultureInfo.InvariantCulture,
+            };
+
+            // The amount is converted to money format based on the culture got in the swith.
+            // To prevent errors the currency simbol is replaced by "" and then the currency parameter is concatenated
+            return cantidadDecimal.ToString("C", culture).Replace(culture.NumberFormat.CurrencySymbol, "") + " " + currecncy;
+        }
+
+    }
+}
