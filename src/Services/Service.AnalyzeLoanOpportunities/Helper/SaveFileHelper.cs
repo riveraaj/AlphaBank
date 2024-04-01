2d526e24124db0094bd3053353d44e428307e28c
riveraaj <jonathandavidr7@gmail.com>
2024-04-01 13:45:59 -0600

    Changed the fullfilePath

--- src/Services/Service.AnalyzeLoanOpportunities/Helper/SaveFileHelper.cs
+++ src/Services/Service.AnalyzeLoanOpportunities/Helper/SaveFileHelper.cs
@@ -4,8 +4,12 @@
         private const string filePath = @"C:\Documents\Proyecto-SoftwareIII\Solicitud-Prestamo";
 
         public static string SaveFile(byte[] pdfContent, string pdfName) {
+            // If the folders doesn´t exists, then they will be created
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+
             //Merge the file name with the directory path to get the full path
-            string fullFilePath = Path.Combine(filePath, pdfName, ".pdf");
+            string fullFilePath = Path.Combine(filePath, pdfName + ".pdf");
 
             //Write the content of the PDF to the specified file
             File.WriteAllBytes(fullFilePath, pdfContent);
