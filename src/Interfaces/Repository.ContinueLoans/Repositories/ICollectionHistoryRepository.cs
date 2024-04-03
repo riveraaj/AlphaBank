993d8f6a3e493b998824085235d9914fa63d9353
riveraaj <jonathandavidr7@gmail.com>
2024-04-03 16:43:43 -0600

    The project and the interface for the ICollectionHistory repository with 3 methods GetAllAsync, CreateAsync and SaveChangesAsync were created.

---
+++ src/Interfaces/Repository.ContinueLoans/Repositories/ICollectionHistoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.AlphaBank;
+
+namespace Repository.ContinueLoans.Repositories {
+    public interface ICollectionHistoryRepository {
+        public Task<ICollection<CollectionHistory>> GetAllAsync();
+        public Task CreateAsync(CollectionHistory oCollectionHistory);
+        public Task SaveChangesAsync();
+    }
+}
