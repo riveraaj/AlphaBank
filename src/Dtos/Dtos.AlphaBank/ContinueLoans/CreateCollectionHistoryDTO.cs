0887cfc6b107fb655abb713bfaab054cfdf8807b
riveraaj <jonathandavidr7@gmail.com>
2024-04-03 16:40:02 -0600

    The dto was created to create a collection history.

---
+++ src/Dtos/Dtos.AlphaBank/ContinueLoans/CreateCollectionHistoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.ContinueLoans {
+    public class CreateCollectionHistoryDTO {
+        [Required]
+        public decimal? DepositAmount { get; set; }
+        [Required]
+        public int? LoanId { get; set; }
+    }
+}
