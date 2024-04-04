4c3e5dfdcbaecd91bb57b6a6d24fd775b2492321
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 16:07:47 -0600

    Created a project and mapper for CollectionHistory

---
+++ src/Mappers/Mapper.ContinueLoans/CollectionHistoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.ContinueLoans;
+
+namespace Mapper.ContinueLoans {
+    public static class CollectionHistoryMapper {
+
+        public static CollectionHistory MapCollectionHistory(CreateCollectionHistoryDTO oCreateCollectionHistoryDTO)
+            => new() {
+                DepositMount = (int) oCreateCollectionHistoryDTO.DepositAmount!,
+                LoanId = (int) oCreateCollectionHistoryDTO.LoanId!,
+            };
+    }
+}
