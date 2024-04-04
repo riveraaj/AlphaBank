c0d16a31269bfc6db3619784f75e0ce9aeab116c
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 16:06:34 -0600

    GetAllForGrantLoan method was added.

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ILoanApplicationService.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ILoanApplicationService.cs
@@ -2,9 +2,8 @@
 
 namespace Interfaces.AnalyzeLoanOpportunities.Services {
     public interface ILoanApplicationService  {
-
+        public Task<List<ShowLoanApplicationDTO>> GetAllForGrantLoan();
         public Task<bool> Create(CreateLoanApplicationDTO oCreateLoanApplicationDTO);
-
         public byte[] ConvertToPdf(FileUploadDTO fileUploadDTO, string title);
     }
 }
