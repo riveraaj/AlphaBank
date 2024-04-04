621efedcbf50b147f89fe27e5dfa17894070ea9a
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 16:05:33 -0600

    Renamed ShowLoanApprovalHistoryDTO class to ShowLoanApplicationDTO.

--- src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/Reports/ShowLoanApprovalHistoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dtos.AlphaBank.Reports {
-    public class ShowLoanApprovalHistoryDTO {
-        public int Id { get; set; }
-        public string TypeLoan { get; set; } = null!;
-        public DateOnly DateApplication { get; set; }
-        public int PersonId { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/ShowLoanApplicationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Dtos.AlphaBank.AnalyzeLoanOpportunities {
+    public class ShowLoanApplicationDTO {
+        public int Id { get; set; }
+        public string TypeLoan { get; set; } = null!;
+        public DateOnly DateApplication { get; set; }
+        public int PersonId { get; set; }
+        public string Amount { get; set; } = null!;
+        public string TypeCurrency { get; set; } = null!;
+    }
+}
