1c20822125285316ed0b199fd64fb2c75cea9887
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 16:05:04 -0600

    The unintentionally deleted method fileUpload has been put back.

--- src/Clients/WebClient/wwwroot/js/site.js
+++ src/Clients/WebClient/wwwroot/js/site.js
@@ -1,92 +1,48 @@
 ﻿//Upload a file
-function fileUpload1() {
-    let files = [];
-    let fileName = document.getElementById('fileEmployerOrder').value.split("\\").pop();
-    files.push(fileName);
+function fileUpload(inputId, ulId) {
+
+    const files = [],
+        fileInput = document.getElementById(inputId),
+        fileList = document.getElementById(ulId),
+        fileName = fileInput.value.split("\\").pop();
+
+    fileList.innerHTML = '';
+
+    files.push(fileName)
     console.log(files);
-    // when the file is uploaded
-    if (document.getElementById('fileEmployerOrder').value != "") {
+
+    //When the file is uploaded
+    if (fileInput.value != "") {
         setTimeout(() => {
-            //           create list of files
-            files.forEach(function (item) {
-                item = item.toLowerCase();
+            //create list of files
+            files.forEach(file => {
+                file = file.toLowerCase();
                 let list = document.createElement("li");
 
-                list.innerText = item;
+                list.innerText = file;
 
-                document.getElementById('orderList').appendChild(list);
+                fileList.appendChild(list);
                 $(list).addClass("li-drag-drop");
-
-            })
+            });
         }, 400);
-    }
-    if (files.length > 0) {
-
-        let reader = new FileReader();
-
-        reader.onload = function (event) {
-            let arrayBuffer = event.target.result;
-
-            console.log(arrayBuffer);
-
-            let byteArray = new Uint8Array(arrayBuffer);
-            console.log(byteArray);
-        };
-
-        let blob = new Blob([files]);
-
-        reader.readAsArrayBuffer(blob);
-    } else {
-        console.error("No se ha seleccionado ningún archivo.");
     }
 }
 
-function fileUpload2() {
-    let files = [];
-    let fileName = document.getElementById('fileSalaryStatement').value.split("\\").pop();
-    files.push(fileName);
-    console.log(files);
-    // when the file is uploaded
-    if (document.getElementById('fileSalaryStatement').value != "") {
-        setTimeout(() => {
-            //           create list of files
-            files.forEach(function (item) {
-                item = item.toLowerCase();
-                let list = document.createElement("li");
-
-                list.innerText = item;
-
-                document.getElementById('salaryList').appendChild(list);
-                $(list).addClass("li-drag-drop");
-
-            })
-        }, 400);
-    }
-    if (files.length > 0) {
-
-        let reader = new FileReader();
-
-        reader.onload = function (event) {
-            let arrayBuffer = event.target.result;
-
-            console.log(arrayBuffer);
-
-            let byteArray = new Uint8Array(arrayBuffer);
-            console.log(byteArray);
-        };
-
-        let blob = new Blob([files]);
-
-        reader.readAsArrayBuffer(blob);
-    } else {
-        console.error("No se ha seleccionado ningún archivo.");
-    }
-}
-
-
-$(document).ready(function () {
-    $('#btnCreateCustomer').click(function () {
+document.addEventListener('DOMContentLoaded', () => {
+    var btnCreateCustomer = document.getElementById('btnCreateCustomer');
+    btnCreateCustomer.addEventListener('click', () => {
         $('#contentCreateCustomer').load('/Customer/showCustomer');
         $('#modalCreateCustomer').modal('show');
     });
 });
+
+//$(document).ready(() => {
+//    var btnCreateCustomer = document.getElementById('btnCreateCustomer');
+//    btnCreateCustomer.addEventListener('click', () => {
+
+//    });
+//    $('#btnCreateCustomer').click(() => {
+//        $('#contentCreateCustomer').load('/Customer/showCustomer');
+//        $('#modalCreateCustomer').modal('show');
+//    });
+//});
