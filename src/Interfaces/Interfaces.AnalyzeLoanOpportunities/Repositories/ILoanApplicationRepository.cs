348c9e290dad7c6ffbe781dda7cf4241c91c9994
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 16:06:18 -0600

    GetAllAsyncForReport method removed

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ILoanApplicationRepository.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ILoanApplicationRepository.cs
@@ -3,7 +3,6 @@
 namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
     public interface ILoanApplicationRepository {
         public Task<ICollection<LoanApplication>> GetAllAsync();
-        public Task<ICollection<LoanApplication>> GetAllAsyncForReport();
         public Task<LoanApplication?> GetByIdForContract(int id);
         public Task Create(LoanApplication oLoanApplication);
         public Task UpdateApplicationStatus(int id, byte statusId);
