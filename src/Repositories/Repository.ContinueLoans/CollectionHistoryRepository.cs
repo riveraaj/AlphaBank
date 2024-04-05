746281d1f2cc41c5fe83a4566cbbf123947545ef
riveraaj <jonathandavidr7@gmail.com>
2024-04-04 23:03:45 -0600

    Create Repository.ContinueLoans project & CollectionHistoryRepository class

---
+++ src/Repositories/Repository.ContinueLoans/CollectionHistoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Microsoft.EntityFrameworkCore;
+using Repository.ContinueLoans.Repositories;
+
+namespace Repository.ContinueLoans{
+    public class CollectionHistoryRepository(AlphaBankDbContext context)
+                                             : ICollectionHistoryRepository{
+
+        private readonly AlphaBankDbContext _context = context;
+
+        public async Task<ICollection<CollectionHistory>> GetAllAsync()
+            => await _context.CollectionHistories.ToListAsync();
+
+
+        public async Task CreateAsync(CollectionHistory oCollectionHistory)
+            => await _context.CollectionHistories.AddAsync(oCollectionHistory);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
