b480594131d94a6f4c4d14ff60bbe14863f76deb
riveraaj <jonathandavidr7@gmail.com>
2024-04-06 20:49:32 -0600

    Removed junk methods

--- src/Clients/WebClient/Controllers/MonitoringLoanStatusController1.cs
+++ src/Clients/WebClient/Controllers/MonitoringLoanStatusController1.cs
@@ -5,49 +5,17 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebClient.Services;
 
-namespace WebClient.Controllers
-{
+namespace WebClient.Controllers {
     public class MonitoringLoanStatusController(IUserAuthenticatorService userAuthenticatorService,
-                                    ICustomerService customerService) : Controller
-    {
+                                    ICustomerService customerService) : Controller {
 
         private readonly IUserAuthenticatorService _userAuthenticatorService = userAuthenticatorService;
         private readonly ICustomerService _customerService = customerService;
         public IActionResult Login() =>
             User.Identity!.IsAuthenticated ? Redirect("~/Home/Index") : View();
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login([Bind("Id", "Password")] UserLoginDTO oUserLoginDTO)
-        {
 
-            if (!ModelState.IsValid) return View();
-
-            var (result, userAuthentication) = await _userAuthenticatorService.UserAuthenticator(oUserLoginDTO);
-
-            if (!result)
-            {
-                ViewData["Error"] = "*Hubo un error en el inicio de sesión, intentelo más tarde.";
-                return View();
-            }
-
-            await CookiesService.CreateAuthenticationCookies(HttpContext, userAuthentication!);
-
-            return Redirect("~/Home/Index");
-        }
-
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            await CookiesService.RemoveAuthenticationCookie(HttpContext);
-            return RedirectToAction("Login");
-        }
-
-        [Authorize(Roles = "1")]
-        public async Task<IActionResult> CustomerList()
-        {
+        public async Task<IActionResult> CustomerList() {
 
 
             View(await _customerService.GetAll());
@@ -59,13 +27,6 @@ namespace WebClient.Controllers
             return View();
         }
 
-
-        [HttpGet]
-        public async Task<IActionResult> MonitoringLoanStatus()
-        {
-
-            return View();
-        }
-
+        public IActionResult MonitoringLoanStatus() => View();     
     }
 }
