5d34c996812d6622db998c7ca9b4c0260691d101
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:31:19 -0600

    Enum created for TypeNotification model

---
+++ src/Models/Data.AlphaBank/Enums/TypeNotificationEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.AlphaBank.Enums {
+    public enum TypeNotificationEnum {
+        RecordatorioDePago = 1,
+        AvisoDeAtrasoEnElPago = 2,
+        AvisoDeProcesoDeCobroJudicial = 3,
+        NotificacionDeFinalizacionDePago = 4,
+        AvisoDeCobroJudicial = 5
+    }
+}
