226a8e3cc3204a896f2c632e81efc92e188c3ef8
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:31:40 -0600

    The mail service configuration template was created.

---
+++ src/Models/Data.Common/MailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Common {
+    public class MailSettings {
+        public string SmtpServer { get; set; } = null!;
+        public int SmtpPort { get; set; }
+        public string SmtpUsername { get; set; } = null!;
+        public string SmtpPassword { get; set; } = null!;
+        public string SenderEmail { get; set; } = null!;
+        public string SenderName { get; set; } = null!;
+    }
+}
