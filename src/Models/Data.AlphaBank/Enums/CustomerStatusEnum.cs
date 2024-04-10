17edf2bb4bf1deab23409972b7266c1dcc2a63c7
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:30:53 -0600

    Enum created for the CustomerStatus model

---
+++ src/Models/Data.AlphaBank/Enums/CustomerStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.AlphaBank.Enums {
+    public enum CustomerStatusEnum{
+        Regular = 1,
+        Suspendido = 2,
+        Cerrado = 3,
+        EnRiesgo = 4,
+        VIP = 5,
+        Moroso = 6
+    }
+}
