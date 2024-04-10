4d7b8571b1a5fa843cce969b01ae449b40bb19e5
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:29:40 -0600

    The interface for NotificationService has been created

---
+++ src/Interfaces/Interfaces.Common/Services/INotificationService.cs
@@ -0,0 +1,8 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common.Services {
+    public interface INotificationService {
+        public Task<bool> Create(Notification oNotification);
+        public Task<string?> GetMessageById(int id);
+    }
+}
