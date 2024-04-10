34f2bc62630b5ba3864049d780686bba0e9515da
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:29:09 -0600

    Created the interface for notifications with 3 methods: CreateAsync, GetMessageByIdAsync, SaveChangesAsync

---
+++ src/Interfaces/Interfaces.Common/Repositories/INotificationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.AlphaBank;
+
+namespace Interfaces.Common.Repositories {
+    public interface INotificationRepository {
+        public Task CreateAsync(Notification oNotification);
+        public Task<string> GetMessageByIdAsync(int id);
+        public Task SaveChangesAsync();
+    }
+}
