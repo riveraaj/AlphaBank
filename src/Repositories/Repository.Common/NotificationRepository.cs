a6f27a1aa17c99eb521a0d7aed16930add4107e0
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:32:03 -0600

    The NotificationRepository was created and all the methods of its interface were implemented.

---
+++ src/Repositories/Repository.Common/NotificationRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data.AlphaBank;
+using Database.AlphaBank;
+using Interfaces.Common.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Common {
+    public class NotificationRepository(AlphaBankDbContext context) : INotificationRepository {
+
+        private readonly AlphaBankDbContext _context = context;
+        
+        public async Task CreateAsync(Notification oNotification)
+            => await _context.Notifications.AddAsync(oNotification);
+
+        public async Task<string> GetMessageByIdAsync(int id)
+            => await _context.TypeNotifications.Where(x => x.Id == id)
+                                               .Select(x => x.Message.Description)
+                                               .SingleOrDefaultAsync()
+                                                ?? " ";
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
