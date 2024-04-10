3131e789f471bbcca1665fc102da7909841d7c86
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:31:00 -0600

    Enum created for LoanStatement model

---
+++ src/Models/Data.AlphaBank/Enums/LoanStatementEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.AlphaBank.Enums {
+    public enum LoanStatementEnum {
+        EnEsperaDePago = 1,
+        RetrasoDePago = 2,
+        EnProcesoCobroJudicial = 3,
+        EnCobroJudicial = 4,
+        Finalizado = 5
+    }
+}
