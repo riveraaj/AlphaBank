be727480128f8616b155ca3ec790f1fc09c03513
riveraaj <jonathandavidr7@gmail.com>
2024-04-09 21:31:07 -0600

    Enum was created for the Role model

---
+++ src/Models/Data.AlphaBank/Enums/RoleEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.AlphaBank.Enums {
+    public enum RoleEnum {
+        Administrador = 1,
+        EspecialistaRecuperacionPrestamos = 2,
+        OficialPrestamos = 3,
+        AnalistaPrestamos = 4,
+        GestorCuentas = 5,
+        GestorSeguimientoPrestamos = 6
+    }
+}
