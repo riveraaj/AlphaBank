86137dd1a5b4b935feb0652d2a72b5eb1e7f7fe0
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-12 02:23:38 +0200

    Añadido el metodo a la interfaz del servicio de Contratos que permite crear un contrato de tipo renegociación

--- src/Interfaces/Interfaces.Common/Services/IContractService.cs
+++ src/Interfaces/Interfaces.Common/Services/IContractService.cs
@@ -4,6 +4,7 @@ using Dtos.AlphaBank.Common;
 namespace Interfaces.Common.Services {
     public interface IContractService {
         public Task<bool> LoanTypeCreate(LoanApplication oLoanApplication);
+        public Task<bool> RenegotiationTypeCreate(Loan oLoan, string newInterestPercentage, string newLoanTerm, decimal newLoanAmount);
         public Task<List<ShowContractDTO>> GetByLoanApplicationID(int id);
         public Task<Contract?> GetByLoanApplicationId(int id);
     }
