702424a41e7afc6518215d710fdcbc2bf303a3d6
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 15:23:14 -0600

    Se crea CreateRenegotiationContractDTO

---
+++ src/Dtos/Dtos.AlphaBank/Common/CreateRenegotiationContractDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Common {
+    public class CreateRenegotiationContractDTO {
+
+        [Required(ErrorMessage = "El campo id es obligatorio")]
+        public int LoanId { get; set; }
+
+        [Required(ErrorMessage = "El campo interés es obligatorio")]
+        public byte? NewInterestPercentage { get; set; }
+        [Required(ErrorMessage = "El campo plazo es obligatorio")]
+        public byte? NewLoanTerm { get; set; }
+        [Required(ErrorMessage = "El campo nuevo monto es obligatorio")]
+        public decimal? NewLoanAmount { get; set; }
+    }
+}
