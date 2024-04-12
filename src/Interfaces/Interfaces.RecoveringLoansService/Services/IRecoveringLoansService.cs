63af3ad1736fdede59655e55d3d7e6114f5d2885
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 15:23:50 -0600

    The interface for the RecoveringLoansService was created with two methods: JudicialLoanProcess & GenerateNewContract.

---
+++ src/Interfaces/Interfaces.RecoveringLoansService/Services/IRecoveringLoansService.cs
@@ -0,0 +1,8 @@
+﻿using Dtos.AlphaBank.Common;
+
+namespace Interfaces.RecoveringLoansService.Services {
+    public interface IRecoveringLoansService {
+        public  Task<bool> JudicialLoanProcess(int loanId, decimal amount);
+        public Task<bool> GenerateNewContract(CreateRenegotiationContractDTO oCreateRenegotiationContractDTO);
+    }
+}
