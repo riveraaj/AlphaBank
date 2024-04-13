e8e3aa754ccd9bc1ed1b069a3b1c84ddae2596eb
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:07:28 -0600

    Added a function to the GetById interface

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ILoanApplicationRepository.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ILoanApplicationRepository.cs
@@ -3,6 +3,7 @@
 namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
     public interface ILoanApplicationRepository {
         public Task<ICollection<LoanApplication>> GetAllAsync();
+        public Task<LoanApplication?> GetById(int id);
         public Task<LoanApplication?> GetByIdForContract(int id);
         public Task Create(LoanApplication oLoanApplication);
         public Task UpdateApplicationStatus(int id, byte statusId);
