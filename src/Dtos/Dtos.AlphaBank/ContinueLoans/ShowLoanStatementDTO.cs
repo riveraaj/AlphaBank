5e59d9ace77f115cac4c4767b4ff4c74215def4c
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:40:31 -0600

    Se crea un nuevo DTO para mostrar prestamos

---
+++ src/Dtos/Dtos.AlphaBank/ContinueLoans/ShowLoanStatementDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Dtos.AlphaBank.ContinueLoans {
+    public  class ShowLoanStatementDTO {
+        public string PersonId { get; set; } = null!;
+        public string LoanStatement { get; set; } = null!;
+        public string CustomerStatus { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
