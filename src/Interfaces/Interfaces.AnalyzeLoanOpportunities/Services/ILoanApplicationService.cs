5b86a4d352881060c4bdb016076f7850e2a608ba
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:07:44 -0600

    Implemented new GetById interface method

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ILoanApplicationService.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ILoanApplicationService.cs
@@ -3,6 +3,7 @@
 namespace Interfaces.AnalyzeLoanOpportunities.Services {
     public interface ILoanApplicationService  {
         public Task<List<ShowLoanApplicationDTO>> GetAllForGrantLoan();
+        public Task<ShowLoanApplicationDTO?> GetById(int id);
         public Task<bool> Create(CreateLoanApplicationDTO oCreateLoanApplicationDTO);
         public byte[] ConvertToPdf(FileUploadDTO fileUploadDTO, string title);
     }
