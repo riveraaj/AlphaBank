da7e749e4effbc9087759f99d7583451a2a5a186
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:04:15 -0600

    A model view was created to load all the data in the GranLoan views.

---
+++ src/Clients/WebClient/Models/GrantLoanViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace WebClient.Models {
+    public class GrantLoanViewModel{
+        public ShowLoanApplicationDTO LoanApplication { get; set; } = null!;
+        public ShowCustomerLoanDTO Customer { get; set; } = null!;
+        public List<ShowLoanApplicationDTO> LoanApplicationList { get; set; } = null!;
+    }
+}
