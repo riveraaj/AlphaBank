35055ce4fceab147b24162e78c9146de50fc9e67
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:40:58 -0600

    New method added: GetAllForContinueLoan

--- src/Interfaces/Interfaces.Common/Services/ILoanService.cs
+++ src/Interfaces/Interfaces.Common/Services/ILoanService.cs
@@ -1,8 +1,10 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.ContinueLoans;
 
 namespace Interfaces.Common.Services {
     public interface ILoanService {
         public Task<List<Loan>> GetAll();
+        public Task<List<ShowLoanStatementDTO>> GetAllForContinueLoan();
         public Task<bool> Create(LoanApplication oLoanApplication);
         public Task UpdateStatement(int id, byte loanStatementId);
         public Task UpdateQuotas(int id);
