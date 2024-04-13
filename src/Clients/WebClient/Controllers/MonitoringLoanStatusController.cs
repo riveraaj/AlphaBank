c779a62089c0c1b141851e1a29cd140e2c8cd097
riveraaj <jonathandavidr7@gmail.com>
2024-04-12 23:40:44 -0600

    All the logic was changed in order to display data.

---
+++ src/Clients/WebClient/Controllers/MonitoringLoanStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Interfaces.Common.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers {
+    public class MonitoringLoanStatusController(ILoanService loanService) : Controller {
+
+        private readonly ILoanService _loanService = loanService;
+
+        public async Task<IActionResult> MonitoringLoanStatus() 
+            => View(await _loanService.GetAllForContinueLoan());     
+    }
+}
