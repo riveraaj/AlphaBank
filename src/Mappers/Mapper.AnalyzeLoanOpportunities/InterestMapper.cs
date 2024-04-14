44eaa5c52d131bc6d7aaae0f53eac28d730ee675
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:00:43 +0200

    Se creo un Mapper para Crear y Actualizar Interest

---
+++ src/Mappers/Mapper.AnalyzeLoanOpportunities/InterestMapper.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace Mapper.AnalyzeLoanOpportunities
+{
+    public static class InterestMapper
+    {
+
+        public static Interest MapInterest(CreateInterestDTO oCreateInterestDTO)
+            => new()
+            {
+                Description = oCreateInterestDTO.Description
+            };
+
+        public static Interest MapUpdateInterest(UpdateInterestDTO oUpdateInterestDTO)
+            => new()
+            {
+                Id = oUpdateInterestDTO.Id,
+                Description = oUpdateInterestDTO.Description
+            };
+    }
+}
