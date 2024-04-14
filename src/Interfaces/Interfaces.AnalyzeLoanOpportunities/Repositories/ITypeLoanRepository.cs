1e084a12a54a9a1bb2d6ce9d30da477e8209dce4
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:21:07 +0200

    Se crearon los metodos necesarios para el CRUD de TypeLoan

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ITypeLoanRepository.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/ITypeLoanRepository.cs
@@ -2,6 +2,11 @@
 
 namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
     public interface ITypeLoanRepository {
+
+        public Task CreateAsync(TypeLoan oTypeLoan);
         public Task<ICollection<TypeLoan>> GetAllAsync();
+        public Task UpdateAsync(TypeLoan oTypeLoan);
+        public Task RemoveAsync(int id);
+        public Task SaveChangesAsync();
     }
 }
