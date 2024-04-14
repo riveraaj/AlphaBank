0064da3779e3272b3ab9f50f526b02e421633e07
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:55:30 +0200

    Se creo el DTO para Crear un nuevo TypeAccount

---
+++ src/Dtos/Dtos.AlphaBank/BankAccounts/CreateTypeAccountDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.BankAccounts
+{
+    public class CreateTypeAccountDTO
+    {
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo descripción tiene que ser menor a 50 carácteres")]
+        public string Description { get; set; } = null!;
+    }
+}
