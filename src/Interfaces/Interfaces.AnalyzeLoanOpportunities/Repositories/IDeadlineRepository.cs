4f4e079695d9f657ee92dcac6d392f77f0abf18c
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:57:52 +0200

    Se agregaron los metodos necesarios a la interfaz del repositorio de Deadline para el CRUD

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IDeadlineRepository.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IDeadlineRepository.cs
@@ -2,6 +2,11 @@
 
 namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
     public interface IDeadlineRepository {
+
+        public Task CreateAsync(Deadline oDeadline);
         public Task<ICollection<Deadline>> GetAllAsync();
+        public Task UpdateAsync(Deadline oDeadline);
+        public Task RemoveAsync(int id);
+        public Task SaveChangesAsync();
     }
 }
