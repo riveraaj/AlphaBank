1d50f16f47ed94b9a9e73730a67274e2688f28b3
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:02:18 +0200

    Se agregaron los metodos necesarios al repositorio de Deadline para el CRUD

--- src/Repositories/Repository.AnalyzeLoanOpportunities/DeadlineRepository.cs
+++ src/Repositories/Repository.AnalyzeLoanOpportunities/DeadlineRepository.cs
@@ -9,7 +9,30 @@ namespace Repository.AnalyzeLoanOpportunities {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task CreateAsync(Deadline oDeadline)
+            => await _context.Deadlines.AddAsync(oDeadline);
+
         public async Task<ICollection<Deadline>> GetAllAsync()
             => await _context.Deadlines.ToListAsync();
+
+        public async Task UpdateAsync(Deadline oDeadline)
+        {
+            var deadline = await _context.Deadlines.FirstOrDefaultAsync(x => x.Id == oDeadline.Id);
+
+            if (deadline == null) return;
+
+            deadline.Description = oDeadline.Description;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            //Search for the record in the table 
+            var deadline = await _context.Deadlines.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (deadline != null) _context.Deadlines.Remove(deadline);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
     }
 }
