d2e5fc2bad4593d3e1206e09a852a5f1cc160c36
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:58:57 +0200

    Se agregaron los metodos necesarios a la interfaz del repositorio de TypeCurrency para el CRUD

--- src/Interfaces/Interfaces.Common/Repositories/ITypeCurrencyRepository.cs
+++ src/Interfaces/Interfaces.Common/Repositories/ITypeCurrencyRepository.cs
@@ -2,6 +2,11 @@
 
 namespace Interfaces.Common.Repositories {
     public interface ITypeCurrencyRepository {
+   
+        public Task CreateAsync(TypeCurrency oTypeCurrency);
         public Task<ICollection<TypeCurrency>> GetAllAsync();
+        public Task UpdateAsync(TypeCurrency oTypeCurrency);
+        public Task RemoveAsync(int id);
+        public Task SaveChangesAsync();
     }
 }
