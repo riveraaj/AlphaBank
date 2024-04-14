09beda7d8f7b9b1acc756875bba15a9f418357a1
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:58:40 +0200

    Se agregaron los metodos necesarios a la interfaz del repositorio de TypeAccount para el CRUD

--- src/Interfaces/Interfaces.BankAccounts/Repositories/ITypeAccountRepository.cs
+++ src/Interfaces/Interfaces.BankAccounts/Repositories/ITypeAccountRepository.cs
@@ -2,7 +2,11 @@
 
 namespace Interfaces.BankAccounts.Repositories {
     public interface ITypeAccountRepository {
-        public Task<ICollection<TypeAccount>> GetAllAsync();
 
+        public Task CreateAsync(TypeAccount oTypeAccount);
+        public Task<ICollection<TypeAccount>> GetAllAsync();
+        public Task UpdateAsync(TypeAccount oTypeAccount);
+        public Task RemoveAsync(int id);
+        public Task SaveChangesAsync();
     }
 }
