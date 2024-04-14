e26dbccba0896144af44541f87fe5f795515fe42
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:21:22 +0200

    Se crearon los metodos necesarios para el CRUD de TypeLoan

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ITypeLoanService.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/ITypeLoanService.cs
@@ -1,7 +1,12 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+using Dtos.AlphaBank.Security;
 
 namespace Interfaces.AnalyzeLoanOpportunities.Services {
     public interface ITypeLoanService {
+        public Task<bool> Create(CreateTypeLoanDTO oCreateTypeLoanDTO);
         public Task<List<TypeLoan>> GetAll();
+        public Task<bool> Update(UpdateTypeLoanDTO oUpdateTypeLoanDTO);
+        public Task<bool> Remove(int id);
     }
 }
