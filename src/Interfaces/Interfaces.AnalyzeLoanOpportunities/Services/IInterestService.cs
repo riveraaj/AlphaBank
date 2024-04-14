20a082ba6341623a8119382a09a4e31d8790e214
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:59:29 +0200

    Se agregaron los metodos necesarios a la interfaz del servicio de Interest para el CRUD

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/IInterestService.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/IInterestService.cs
@@ -1,7 +1,11 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
 
 namespace Interfaces.AnalyzeLoanOpportunities.Services {
     public interface IInterestService {
+        public Task<bool> Create(CreateInterestDTO oCreateInterestDTO);
         public Task<List<Interest>> GetAll();
+        public Task<bool> Update(UpdateInterestDTO oUpdateInterestDTO);
+        public Task<bool> Remove(int id);
     }
 }
