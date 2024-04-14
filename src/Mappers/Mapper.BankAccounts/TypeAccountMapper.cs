4edc076307e1bffff4c69f198e3b302f56d57cfe
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:00:57 +0200

    Se creo un Mapper para Crear y Actualizar TypeAccount

---
+++ src/Mappers/Mapper.BankAccounts/TypeAccountMapper.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.BankAccounts;
+
+namespace Mapper.BankAccounts
+{
+    public static class TypeAccountMapper
+    {
+
+        public static TypeAccount MapTypeAccount(CreateTypeAccountDTO oCreateTypeAccountDTO)
+            => new()
+            {
+                Description = oCreateTypeAccountDTO.Description
+            };
+
+        public static TypeAccount MapUpdateTypeAccount(UpdateTypeAccountDTO oUpdateTypeAccountDTO)
+            => new()
+            {
+                Id = oUpdateTypeAccountDTO.Id,
+                Description = oUpdateTypeAccountDTO.Description
+            };
+    }
+}
