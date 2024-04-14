91b8e75c5d7ce1eca91f88fe41528f73ffcd56d6
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:01:09 +0200

    Se creo un Mapper para Crear y Actualizar TypeCurrency

---
+++ src/Mappers/Mapper.Common/TypeCurrencyMapper.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Common;
+
+namespace Mapper.Common
+{
+    public static class TypeCurrencyMapper
+    {
+
+        public static TypeCurrency MapTypeCurrency(CreateTypeCurrencyDTO oCreateTypeCurrencyDTO)
+            => new()
+            {
+                Description = oCreateTypeCurrencyDTO.Description
+            };
+
+        public static TypeCurrency MapUpdateTypeCurrency(UpdateTypeCurrencyDTO oUpdateTypeCurrencyDTO)
+            => new()
+            {
+                Id = oUpdateTypeCurrencyDTO.Id,
+                Description = oUpdateTypeCurrencyDTO.Description
+            };
+    }
+}
