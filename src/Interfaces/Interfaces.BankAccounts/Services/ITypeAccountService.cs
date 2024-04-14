aba73fc404d119a3956ca624b069f1affb8aaec9
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:59:47 +0200

    Se agregaron los metodos necesarios a la interfaz del servicio de TypeAccount para el CRUD

--- src/Interfaces/Interfaces.BankAccounts/Services/ITypeAccountService.cs
+++ src/Interfaces/Interfaces.BankAccounts/Services/ITypeAccountService.cs
@@ -1,7 +1,11 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.BankAccounts;
 
 namespace Interfaces.BankAccounts.Services {
     public interface ITypeAccountService {
+        public Task<bool> Create(CreateTypeAccountDTO oCreateTypeAccountDTO);
         public Task<List<TypeAccount>> GetAll();
+        public Task<bool> Update(UpdateTypeAccountDTO oUpdateTypeAccountDTO);
+        public Task<bool> Remove(int id);
     }
 }
