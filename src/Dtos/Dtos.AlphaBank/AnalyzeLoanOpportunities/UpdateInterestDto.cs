1a94b77cb9887758d2cddbf8c40e92718ba7f119
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:56:20 +0200

    Se creo el DTO para Actualizar un Interest

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/UpdateInterestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.AnalyzeLoanOpportunities
+{
+    public class UpdateInterestDTO
+    {
+        public byte Id { get; set; }
+
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(25, ErrorMessage = "El campo descripción tiene que ser menor a 25 carácteres")]
+        [Range(1, 99, ErrorMessage = "El campo descripción debe ser un número entero mayor a 0 y menor que 100")]
+        public string Description { get; set; } = null!;
+    }
+}
