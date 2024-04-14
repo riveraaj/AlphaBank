802e97c96dea715e69f9c347b1902110828b7585
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:20:34 +0200

    Se agrego una validacion faltante del maximo de caracteres

--- src/Dtos/Dtos.AlphaBank/Security/CreateRoleDto.cs
+++ src/Dtos/Dtos.AlphaBank/Security/CreateRoleDto.cs
@@ -4,6 +4,7 @@ namespace Dtos.AlphaBank.Security {
     public class CreateRoleDTO {
 
         [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo descripción tiene que ser menor a 50 carácteres")]
         public string Description { get; set; } = null!;
     }
 }
