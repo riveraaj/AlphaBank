18fedc4d08b59e80417b4ce9424da60e3e871a37
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:56:53 +0200

    Se creo el DTO para Actualizar un TypeCurrency

---
+++ src/Dtos/Dtos.AlphaBank/Common/UpdateTypeCurrencyDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Common
+{
+    public class UpdateTypeCurrencyDTO
+    {
+        public byte Id { get; set; }
+
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(3, ErrorMessage = "El campo descripción tiene que ser menor a 3 carácteres")]
+        [RegularExpression("^[A-Z]+$", ErrorMessage = "El campo descripción debe contener solo letras mayúsculas.")]
+        public string Description { get; set; } = null!;
+    }
+}
