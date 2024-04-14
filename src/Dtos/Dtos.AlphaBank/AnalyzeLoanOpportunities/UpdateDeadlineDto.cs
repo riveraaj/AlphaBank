2354067fba6de4f64694c3f13b65f23b643cc2bd
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:56:05 +0200

    Se creo el DTO para Actualizar un Deadline

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/UpdateDeadlineDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.AnalyzeLoanOpportunities
+{
+    public class UpdateDeadlineDTO
+    {
+        public byte Id { get; set; }
+
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(25, ErrorMessage = "El campo descripción tiene que ser menor a 25 carácteres")]
+        [RegularExpression(@"^(?!0 Meses$)\d+ Meses$",
+            ErrorMessage = "El campo descripción debe tener el formato '# Meses', por ejemplo '128 Meses' y no puede ser '0 Meses'")]
+        public string Description { get; set; } = null!;
+
+    }
+}
