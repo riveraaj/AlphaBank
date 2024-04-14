03a09aa899417e1dc9db654547c5c88e695fcc75
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:55:43 +0200

    Se creo el DTO para Crear un nuevo TypeCurrency

---
+++ src/Dtos/Dtos.AlphaBank/Common/CreateTypeCurrencyDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Common
+{
+    public class CreateTypeCurrencyDTO
+    {
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(3, ErrorMessage = "El campo descripción tiene que ser menor a 3 carácteres")]
+        [RegularExpression("^[A-Z]+$", ErrorMessage = "El campo descripción debe contener solo letras mayúsculas.")]
+        public string Description { get; set; } = null!;
+    }
+}
