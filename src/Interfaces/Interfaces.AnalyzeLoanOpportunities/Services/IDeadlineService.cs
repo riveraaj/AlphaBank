05387688d52c9bc0e151e8687d1641ff170c4fc2
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:59:16 +0200

    Se agregaron los metodos necesarios a la interfaz del servicio de Deadline para el CRUD

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/IDeadlineService.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Services/IDeadlineService.cs
@@ -1,7 +1,11 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
 
 namespace Interfaces.AnalyzeLoanOpportunities.Services {
     public interface IDeadlineService {
+        public Task<bool> Create(CreateDeadlineDTO oCreateDeadlineDTO);
         public Task<List<Deadline>> GetAll();
+        public Task<bool> Update(UpdateDeadlineDTO oUpdateDeadlineDTO);
+        public Task<bool> Remove(int id);
     }
 }
