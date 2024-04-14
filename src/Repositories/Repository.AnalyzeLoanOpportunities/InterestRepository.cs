d79bf2ec6f4c9b256e7e95b5cdfb947b0cc91fcf
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:02:33 +0200

    Se agregaron los metodos necesarios al repositorio de Interest para el CRUD

--- src/Repositories/Repository.AnalyzeLoanOpportunities/InterestRepository.cs
+++ src/Repositories/Repository.AnalyzeLoanOpportunities/InterestRepository.cs
@@ -9,7 +9,30 @@ namespace Repository.AnalyzeLoanOpportunities {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task CreateAsync(Interest oInterest)
+            => await _context.Interests.AddAsync(oInterest);
+
         public async Task<ICollection<Interest>> GetAllAsync()
             => await _context.Interests.ToListAsync();
+
+        public async Task UpdateAsync(Interest oInterest)
+        {
+            var interest = await _context.Interests.FirstOrDefaultAsync(x => x.Id == oInterest.Id);
+
+            if (interest == null) return;
+
+            interest.Description = oInterest.Description;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            //Search for the record in the table 
+            var interest = await _context.Interests.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (interest != null) _context.Interests.Remove(interest);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
     }
 }
