46134dd0d12315c06f564c48992814daea9b5621
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:03:01 +0200

    Se agregaron los metodos necesarios al repositorio de TypeCurrency para el CRUD

--- src/Repositories/Repository.Common/TypeCurrencyRepository.cs
+++ src/Repositories/Repository.Common/TypeCurrencyRepository.cs
@@ -9,7 +9,30 @@ namespace Repository.Common {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task CreateAsync(TypeCurrency oTypeCurrency)
+            => await _context.TypeCurrencies.AddAsync(oTypeCurrency);
+
         public async Task<ICollection<TypeCurrency>> GetAllAsync()
             => await _context.TypeCurrencies.ToListAsync();
+
+        public async Task UpdateAsync(TypeCurrency oTypeCurrency)
+        {
+            var typeCurrency = await _context.TypeCurrencies.FirstOrDefaultAsync(x => x.Id == oTypeCurrency.Id);
+
+            if (typeCurrency == null) return;
+
+            typeCurrency.Description = oTypeCurrency.Description;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            //Search for the record in the table 
+            var typeCurrency = await _context.TypeCurrencies.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (typeCurrency != null) _context.TypeCurrencies.Remove(typeCurrency);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
     }
 }
