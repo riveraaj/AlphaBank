36dc9e61781ae4d3ad3de11d5dd3c14a11ed6e29
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:00:03 +0200

    Se agregaron los metodos necesarios a la interfaz del servicio de TypeCurrency para el CRUD

--- src/Interfaces/Interfaces.Common/Services/ITypeCurrencyService.cs
+++ src/Interfaces/Interfaces.Common/Services/ITypeCurrencyService.cs
@@ -1,7 +1,11 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Common;
 
 namespace Interfaces.Common.Services {
     public interface ITypeCurrencyService {
+        public Task<bool> Create(CreateTypeCurrencyDTO oCreateTypeCurrencyDTO);
         public Task<List<TypeCurrency>> GetAll();
+        public Task<bool> Update(UpdateTypeCurrencyDTO oUpdateTypeCurrencyDTO);
+        public Task<bool> Remove(int id);
     }
 }
