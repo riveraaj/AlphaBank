a46a088cb839c578a0e713dc08015ff568887de0
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:00:32 +0200

    Se creo un Mapper para Crear y Actualizar Deadline

---
+++ src/Mappers/Mapper.AnalyzeLoanOpportunities/DeadlineMapper.cs
@@ -0,0 +1,22 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace Mapper.AnalyzeLoanOpportunities
+{
+    public static class DeadlineMapper
+    {
+        public static Deadline MapDeadline(CreateDeadlineDTO oCreateDeadlineDTO)
+            => new()
+            {
+                Description = oCreateDeadlineDTO.Description
+            };
+
+        public static Deadline MapUpdateDeadline(UpdateDeadlineDTO oUpdateDeadlineDTO)
+            => new()
+            {
+                Id = oUpdateDeadlineDTO.Id,
+                Description = oUpdateDeadlineDTO.Description
+            };
+
+    }
+}
