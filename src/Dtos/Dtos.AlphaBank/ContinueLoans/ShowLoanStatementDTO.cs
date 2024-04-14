052afe886f00fdc7967488774f4c090ea6d3bb01
riveraaj <jonathandavidr7@gmail.com>
2024-04-13 19:42:18 -0600

    Added two attributes to the DTO: LoanId & FullName

--- src/Dtos/Dtos.AlphaBank/ContinueLoans/ShowLoanStatementDTO.cs
+++ src/Dtos/Dtos.AlphaBank/ContinueLoans/ShowLoanStatementDTO.cs
@@ -1,6 +1,8 @@
 ﻿namespace Dtos.AlphaBank.ContinueLoans {
     public  class ShowLoanStatementDTO {
+        public string LoanId { get; set; } = null!;
         public string PersonId { get; set; } = null!;
+        public string FullName { get; set; } = null!;
         public string LoanStatement { get; set; } = null!;
         public string CustomerStatus { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
