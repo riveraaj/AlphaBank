f781b92fbd6ba08ff72640ff88f906b22a77aa34
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 01:02:49 +0200

    Se agregaron los metodos necesarios al repositorio de TypeAccount para el CRUD

--- src/Repositories/Repository.BankAccounts/TypeAccountRepository.cs
+++ src/Repositories/Repository.BankAccounts/TypeAccountRepository.cs
@@ -9,7 +9,30 @@ namespace Repository.BankAccounts {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task CreateAsync(TypeAccount oTypeAccount)
+            => await _context.TypeAccounts.AddAsync(oTypeAccount);
+
         public async Task<ICollection<TypeAccount>> GetAllAsync()
             => await _context.TypeAccounts.ToListAsync();
+
+        public async Task UpdateAsync(TypeAccount oTypeAccount)
+        {
+            var typeAccount = await _context.TypeAccounts.FirstOrDefaultAsync(x => x.Id == oTypeAccount.Id);
+
+            if (typeAccount == null) return;
+
+            typeAccount.Description = oTypeAccount.Description;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            //Search for the record in the table 
+            var typeAccount = await _context.TypeAccounts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (typeAccount != null) _context.TypeAccounts.Remove(typeAccount);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
     }
 }
