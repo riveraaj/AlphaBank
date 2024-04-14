b91a3ab1314de57fc83025278940e80db927082c
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:23:14 +0200

    Se crearon los metodos necesarios para el repositorio para el CRUD de TypeLoan

--- src/Repositories/Repository.AnalyzeLoanOpportunities/TypeLoanRepository.cs
+++ src/Repositories/Repository.AnalyzeLoanOpportunities/TypeLoanRepository.cs
@@ -9,7 +9,30 @@ namespace Repository.AnalyzeLoanOpportunities {
 
         private readonly AlphaBankDbContext _context = context;
 
+        public async Task CreateAsync(TypeLoan oTypeLoan)
+            => await _context.TypeLoans.AddAsync(oTypeLoan);
+
         public async Task<ICollection<TypeLoan>> GetAllAsync()
             => await _context.TypeLoans.ToListAsync();
+
+        public async Task UpdateAsync(TypeLoan oTypeLoan)
+        {
+            var typeLoan = await _context.TypeLoans.FirstOrDefaultAsync(x => x.Id == oTypeLoan.Id);
+
+            if (typeLoan == null) return;
+
+            typeLoan.Description = oTypeLoan.Description;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            //Search for the record in the table 
+            var typeLoan = await _context.TypeLoans.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (typeLoan != null) _context.TypeLoans.Remove(typeLoan);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
     }
 }
