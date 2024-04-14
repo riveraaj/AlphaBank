1ebe834886af5d5f86ecd6650b076400858c5bfe
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:19:27 +0200

    Se creo el DTO para Crear un TypeLoan

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/CreateTypeLoanDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.AnalyzeLoanOpportunities
+{
+    public class CreateTypeLoanDTO
+    {
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(25, ErrorMessage = "El campo descripción tiene que ser menor a 25 carácteres")]
+        public string Description { get; set; } = null!;
+    }
+}
