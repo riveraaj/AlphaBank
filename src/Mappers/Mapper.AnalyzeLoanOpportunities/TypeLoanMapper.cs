2f17b33198afa00ca59b391321979088fe66be0d
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:24:38 +0200

    Se creo el Mapper de TypeLoan con un metodo para crear y otro para actualizar

---
+++ src/Mappers/Mapper.AnalyzeLoanOpportunities/TypeLoanMapper.cs
@@ -0,0 +1,23 @@
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+
+namespace Mapper.AnalyzeLoanOpportunities
+{
+    public static class TypeLoanMapper
+    {
+        public static TypeLoan MapTypeLoan(CreateTypeLoanDTO oCreateTypeLoaneDTO)
+            => new()
+            {
+                Description = oCreateTypeLoaneDTO.Description
+            };
+
+        public static TypeLoan MapUpdateTypeLoan(UpdateTypeLoanDTO oUpdateTypeLoanDTO)
+            => new()
+            {
+                Id = oUpdateTypeLoanDTO.Id,
+                Description = oUpdateTypeLoanDTO.Description
+            };
+
+
+    }
+}
