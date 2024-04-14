c6173f3f1cbd79585dfe401d46efc4b8934d80ad
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:19:48 +0200

    Se creo el DTO para Actualizar un TypeLoan

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/UpdateTypeLoanDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.AnalyzeLoanOpportunities
+{
+    public class UpdateTypeLoanDTO
+    {
+        public byte Id { get; set; }
+
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(25, ErrorMessage = "El campo descripción tiene que ser menor a 25 carácteres")]
+        public string Description { get; set; } = null!;
+    }
+}
