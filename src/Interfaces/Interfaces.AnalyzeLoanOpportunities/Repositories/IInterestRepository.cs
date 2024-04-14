2b3aab54c106d529269ff1dbd8697989b5d71835
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:58:11 +0200

    Se agregaron los metodos necesarios a la interfaz del repositorio de Interest para el CRUD

--- src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IInterestRepository.cs
+++ src/Interfaces/Interfaces.AnalyzeLoanOpportunities/Repositories/IInterestRepository.cs
@@ -2,6 +2,11 @@
 
 namespace Interfaces.AnalyzeLoanOpportunities.Repositories {
     public interface IInterestRepository {
+
+        public Task CreateAsync(Interest oInterest);
         public Task<ICollection<Interest>> GetAllAsync();
+        public Task UpdateAsync(Interest oInterest);
+        public Task RemoveAsync(int id);
+        public Task SaveChangesAsync();
     }
 }
