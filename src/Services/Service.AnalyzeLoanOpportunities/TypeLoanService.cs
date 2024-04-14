3eec1e3b3ca6576649699a5bcc6944ebbbabdb68
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:23:28 +0200

    Se crearon los metodos necesarios para el servicio para el CRUD de TypeLoan

--- src/Services/Service.AnalyzeLoanOpportunities/TypeLoanService.cs
+++ src/Services/Service.AnalyzeLoanOpportunities/TypeLoanService.cs
@@ -1,20 +1,114 @@
 ﻿using Data.AlphaBank;
+using Dtos.AlphaBank.AnalyzeLoanOpportunities;
 using Interfaces.AnalyzeLoanOpportunities.Repositories;
 using Interfaces.AnalyzeLoanOpportunities.Services;
+using Mapper.AnalyzeLoanOpportunities;
+using Microsoft.Extensions.Logging;
 
-namespace Service.AnalyzeLoanOpportunities {
-    public class TypeLoanService(ITypeLoanRepository typeLoanRepository) : ITypeLoanService {
+namespace Service.AnalyzeLoanOpportunities
+{
+    public class TypeLoanService(ITypeLoanRepository typeLoanRepository, ILogger<TypeLoanService> logger) : ITypeLoanService
+    {
 
         private readonly ITypeLoanRepository _typeLoanRepository = typeLoanRepository;
+        private readonly ILogger<TypeLoanService> _logger = logger;
 
-        public async Task<List<TypeLoan>> GetAll() {
-            try  {
-                //Attempt to retrieve all typeLoan asynchronously from the RoleRepository.
+        public async Task<bool> Create(CreateTypeLoanDTO oCreateTypeLoanDTO)
+        {
+            // Map CreateTypeLoanDto to a type loan object using some mapper (TypeLoanMapper)
+            var typeLoan = TypeLoanMapper.MapTypeLoan(oCreateTypeLoanDTO);
+
+            try
+            {
+
+                _logger.LogInformation("----- Create TypeLoan: Start creating and saving the type of loan in the database");
+
+                // Attempt to add the new type of loan through the TypeLoanRepository and save changes asynchronously.
+                await _typeLoanRepository.CreateAsync(typeLoan);
+
+                await _typeLoanRepository.SaveChangesAsync();
+
+                _logger.LogInformation("----- Create TypeLoan: Successfully completes the process");
+
+                // Return true to indicate successful creation.
+                return true;
+            }
+            catch
+            {
+                _logger.LogError("----- Create TypeLoan: An error occurred while creating and saving to the database.");
+                //If there's an exception during the process, return false.
+                return false;
+            }
+        }
+
+        public async Task<List<TypeLoan>> GetAll()
+        {
+            try
+            {
+                //Attempt to retrieve all typeLoan asynchronously from the TypeLoanRepository.
                 return (List<TypeLoan>)await _typeLoanRepository.GetAllAsync();
-            } catch {
+            }
+            catch
+            {
                 //If there's an exception during the process, return null.
                 return [];
             }
         }
+
+        public async Task<bool> Update(UpdateTypeLoanDTO oUpdateTypeLoanDTO)
+        {
+            // Map UpdateTypeLoanDto to a type of loan object using some mapper (TypeLoanMapper)
+            var typeLoan = TypeLoanMapper.MapUpdateTypeLoan(oUpdateTypeLoanDTO);
+
+            try
+            {
+
+                _logger.LogInformation("----- Update TypeLoan: Start updating and saving the type of loan in the database");
+
+                // Attempt to update a type of loan through the TypeLoanRepository and save changes asynchronously.
+                await _typeLoanRepository.UpdateAsync(typeLoan);
+
+                await _typeLoanRepository.SaveChangesAsync();
+
+                _logger.LogInformation("----- Update TypeLoan: Successfully completes the process");
+
+                // Return true to indicate successful update.
+                return true;
+            }
+            catch
+            {
+                _logger.LogError("----- Update TypeLoan: An error occurred while updating and saving to the database.");
+                //If there's an exception during the process, return false.
+                return false;
+
+
+            }
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            try
+            {
+                _logger.LogInformation("----- Remove TypeLoan: Start removing a type of loan and saving the changes in the database");
+
+                // Attempt to remove a type of loan through the TypeLoanRepository and save changes asynchronously.
+                await _typeLoanRepository.RemoveAsync(id);
+
+                await _typeLoanRepository.SaveChangesAsync();
+
+                _logger.LogInformation("----- Remove TypeLoan: Successfully completes the process");
+
+                // Return true to indicate successful update.
+                return true;
+            }
+            catch
+            {
+                _logger.LogError("----- Remove TypeLoan: An error occurred while removing a type of loan and saving the changes in the database.");
+                //If there's an exception during the process, return false.
+                return false;
+
+            }
+        }
+
     }
 }
