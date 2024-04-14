9d3f9807d3ba1aa55ff35d2a1e0f60a1d375bb5c
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-14 23:20:06 +0200

    Se creo el DTO para Actualizar un Role

---
+++ src/Dtos/Dtos.AlphaBank/Security/UpdateRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.Security
+{
+    public class UpdateRoleDTO
+    {
+        public byte Id { get; set; }
+
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo descripción tiene que ser menor a 50 carácteres")]
+        public string Description { get; set; } = null!;
+    }
+}
