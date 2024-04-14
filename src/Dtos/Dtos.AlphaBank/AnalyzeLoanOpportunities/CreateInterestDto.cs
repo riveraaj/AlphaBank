2845763aeb98cbc8346f2f09c4ac52b1bbc5db9f
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 00:55:16 +0200

    Se creo el DTO para Crear un nuevo Interest

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/CreateInterestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.AnalyzeLoanOpportunities
+{
+    public class CreateInterestDTO
+    {
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [MaxLength(25, ErrorMessage = "El campo descripción tiene que ser menor a 25 carácteres")]
+        [Range(1, 99, ErrorMessage = "El campo descripción debe ser un número entero mayor a 0 y menor que 100")]
+        public string Description { get; set; } = null!;
+    }
+}
