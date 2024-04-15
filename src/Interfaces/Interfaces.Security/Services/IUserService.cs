11fa10502c328c78e50699056807c828259c72b7
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 02:48:44 +0200

    Se agregaron 2 metodos en la interfaz del servicio para Actualizar y Eliminar Usuarios

--- src/Interfaces/Interfaces.Security/Services/IUserService.cs
+++ src/Interfaces/Interfaces.Security/Services/IUserService.cs
@@ -5,5 +5,7 @@ namespace Interfaces.Security.Services {
     public interface IUserService {
         public Task<bool> UserSetup(CreateEmployeeDTO oCreateEmployeeDTO, IEmployeeRepository oEmployeeRepository);
         public Task<bool> Create(CreateUserDTO oCreateUserDTO);
+        public Task<bool> Update(UpdateUserDTO oUpdateUserDTO);
+        public Task<bool> Remove(int id);
     }
 }
