8b56ab7916826ab7c5d277fc2fbcf6dc16baea62
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-15 03:27:36 +0200

    Se creo el DTO para Actualizar un Customer

---
+++ src/Dtos/Dtos.AlphaBank/BankAccounts/UpdateCustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos.AlphaBank.BankAccounts
+{
+    public class UpdateCustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo correo es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$",
+            ErrorMessage = "El formato del correo no es válido.")]
+        [MaxLength(200, ErrorMessage = "El campo correo tiene que ser menor a 200 caracteres")]
+        public string EmailAddress { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo salario promedio mensual es obligatorio.")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "El campo salario promedio mensual debe ser un número válido.")]
+        public decimal? AverageMonthlySalary { get; set; }
+
+        [Required(ErrorMessage = "El campo ocupación es obligatorio.")]
+        public byte? OccupationId { get; set; }
+
+        [Required(ErrorMessage = "El campo estado es obligatorio.")]
+        public byte? CustomerStatusId { get; set; }
+    }
+}
