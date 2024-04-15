c40fe023d7de0b0953ae847c9d25ac677074dbf0
riveraaj <jonathandavidr7@gmail.com>
2024-04-15 16:19:35 -0600

    Added logic to update and delete clients

--- src/Clients/WebClient/Controllers/CustomerController.cs
+++ src/Clients/WebClient/Controllers/CustomerController.cs
@@ -1,8 +1,10 @@
-﻿using Dtos.AlphaBank.BankAccounts;
+﻿using Data.AlphaBank.Enums;
+using Dtos.AlphaBank.BankAccounts;
 using Interfaces.BankAccounts.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Scripting;
 using WebClient.Services;
 
 namespace WebClient.Controllers {
@@ -21,7 +23,6 @@ namespace WebClient.Controllers {
             return View(await _customerService.GetAll());
         }
         public async Task<ActionResult> ShowCustomer() {
-
                 //We obtain all the lists of data to be used in the select forms formatted in
                 //SelectList to enter them in a ViewData
                 var typeIdentificationList = await _commonService.GetTypeIdentificationSelectListItems();
@@ -36,16 +37,25 @@ namespace WebClient.Controllers {
                 ViewData["MaritalStatus"] = new SelectList(maritalStatusList, "Value", "Text");
                 ViewData["Occupation"] = new SelectList(occupationList, "Value", "Text");
                 ViewData["TypePhone"] = new SelectList(typePhoneList, "Value", "Text");
-                return PartialView("CreateCustomer");
-                // Modificar
-               
-                
+                return PartialView("CreateCustomer");   
         }
+
         public async Task<ActionResult> ShowCustomerUpdate(int? id){
 
             if (!id.HasValue) return PartialView("UpdateCustomer");
            
-            var customer = await _customerService.GetByIdForAccount(id.Value);
+            var customer = await _customerService.GetByIdForUpdate(id.Value);
+
+            if (customer == null) return PartialView("UpdateCustomer");
+
+            //We obtain all the lists of data to be used in the select forms formatted in
+            //SelectList to enter them in a ViewData
+            var statusList = _commonService.GetStatusSelectListItems();
+            var occupationList = await _commonService.GetOccupationSelectListItems();
+
+            //ViewData is created for each SelectList and formatted as follows
+            ViewData["Status"] = new SelectList(statusList, "Value", "Text", occupationList.FirstOrDefault(x => x.Value.Equals(customer.CustomerStatusId)));
+            ViewData["Occupation"] = new SelectList(occupationList, "Value", "Text", occupationList.FirstOrDefault(x => x.Value.Equals(customer.OccupationId)));
 
             return PartialView("UpdateCustomer", customer);
         }
@@ -73,6 +83,58 @@ namespace WebClient.Controllers {
                 return RedirectToAction("CustomerList");
             }
             script = "<script>AlertSuccess('Cliente creada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("CustomerList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateCustomerDTO oUpdateCustomerDTO) {
+
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el cliente, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("CustomerList");
+            }
+
+            var result = await _customerService.Update(oUpdateCustomerDTO);
+
+            if (!result){
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el cliente, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("CustomerList");
+            }
+
+            script = "<script>AlertSuccess('Cliente actualizado exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("CustomerList");
+        }
+
+        public async Task<ActionResult> Delete(int id)  {
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido inactivar el cliente, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("CustomerList");
+            }
+
+            var result = await _customerService.Remove(id);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido inactivar el cliente, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("CustomerList");
+            }
+            script = "<script>AlertSuccess('Cliente inactivado exitosamente','');</script>";
 
             TempData["AlertSuccess"] = script;
             return RedirectToAction("CustomerList");
