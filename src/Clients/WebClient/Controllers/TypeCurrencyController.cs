2158ecd594114e3725431dcc8053745101e479f0
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 13:03:21 -0600

    All the logic for creating, deleting and updating the model has been created.

--- src/Clients/WebClient/Controllers/TypeCurrencyController.cs
+++ src/Clients/WebClient/Controllers/TypeCurrencyController.cs
@@ -1,36 +1,112 @@
-﻿using Dtos.AlphaBank.Security;
-using Interfaces.Security.Services;
-using Interfaces.BankAccounts.Services;
+﻿using Dtos.AlphaBank.Common;
+using Interfaces.Common.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using WebClient.Services;
-using WebClient.Models;
-using Data.AlphaBank;
-using Service.BankAccounts;
 
-namespace WebClient.Controllers
+namespace WebClient.Controllers {
+    [Authorize]
+    public class TypeCurrencyController(ITypeCurrencyService typeCurrencyService) : Controller {
 
-{
-    public class TypeCurrencyController() : Controller
-    {
+        private readonly ITypeCurrencyService _typeCurrencyService = typeCurrencyService;
 
-        
+        public async Task<IActionResult> TypeCurrencyList() {
+            if (TempData.TryGetValue("AlertError", out object? error)) ViewBag.AlertError = error;
+            if (TempData.TryGetValue("AlertSuccess", out object? success)) ViewBag.AlertSuccess = success;
 
-        [Authorize(Roles = "1")]
-        public async Task<IActionResult> TypeCurrencyList()
-        {
-
-            return View();
+            return View(await _typeCurrencyService.GetAllForShow());
         }
 
+        public IActionResult CreateTypeCurrency()
+           => PartialView("CreateTypeCurrency");
 
-        public ActionResult showTypeCurrency()
-        {
-            return PartialView("CreateTypeCurrency");
+        public async Task<IActionResult> UpdateTypeCurrency(int? id)  {
+
+            if (!id.HasValue) return PartialView("UpdateTypeCurrency");
+
+            var typeCurrency = await _typeCurrencyService.GetById((int)id);
+
+            if (typeCurrency == null) return PartialView("UpdateTypeCurrency");
+
+            return PartialView("UpdateTypeCurrency", typeCurrency);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateTypeCurrencyDTO oCreateTypeCurrencyDTO) {
+            string script;
 
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+
+            var result = await _typeCurrencyService.Create(oCreateTypeCurrencyDTO);
+
+            if (!result)
+            {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+            script = "<script>AlertSuccess('Moneda creada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeCurrencyList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateTypeCurrencyDTO oUpdateTypeCurrencyDTO)  {
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+
+            var result = await _typeCurrencyService.Update(oUpdateTypeCurrencyDTO);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+            script = "<script>AlertSuccess('Moneda actualizado exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeCurrencyList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id) {
+            string script;
+
+            if (!id.HasValue) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+
+            var result = await _typeCurrencyService.Remove((int)id);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar la moneda, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeCurrencyList");
+            }
+            script = "<script>AlertSuccess('Moneda eliminado exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeCurrencyList");
+        }
     }
 }
-
-
