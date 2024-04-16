16fd6ef8954ddf88422cf81342d330c17b4999ef
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 02:16:59 -0600

    This class was created to provide data to the selects.

---
+++ src/Clients/WebClient/Services/SecurityService.cs
@@ -0,0 +1,31 @@
+﻿using Interfaces.Security.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebClient.Services {
+    public class SecurityService(IEmployeeService employeeService,
+                                 IRoleService roleService) {
+
+        private readonly IEmployeeService _employeeService = employeeService;
+        private readonly IRoleService _roleService = roleService;
+
+        public async Task<IEnumerable<SelectListItem>> GetEmployeeSelectListItems() {
+
+            var employeeList = await _employeeService.GetAllForUser();
+
+            return employeeList.Select(x => new SelectListItem {
+                Value = x.Id.ToString(),
+                Text = $"{x.Person.Name} {x.Person.FirstName} {x.Person.SecondName}"
+            });
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetRoleSelectListItems() {
+
+            var roleList = await _roleService.GetAll();
+
+            return roleList.Select(x => new SelectListItem {
+                Value = x.Id.ToString(),
+                Text = x.Description
+            });
+        }
+    }
+}
