50502399b86d8693890bcf85f4ffc85f6436be36
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 13:03:17 -0600

    All the logic for creating, deleting and updating the model has been created.

--- src/Clients/WebClient/Controllers/TypeAccountController.cs
+++ src/Clients/WebClient/Controllers/TypeAccountController.cs
@@ -1,38 +1,112 @@
-﻿using Dtos.AlphaBank.Security;
-using Interfaces.Security.Services;
+﻿using Dtos.AlphaBank.BankAccounts;
 using Interfaces.BankAccounts.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using WebClient.Services;
-using WebClient.Models;
-using Data.AlphaBank;
-using Service.BankAccounts;
 
-namespace WebClient.Controllers
+namespace WebClient.Controllers {
+    [Authorize]
+    public class TypeAccountController(ITypeAccountService typeAccountService) : Controller {
 
-{
-    public class TypeAccountController() : Controller
-    {
+        private readonly ITypeAccountService _typeAccountService = typeAccountService;
 
-        
+        public async Task<IActionResult> TypeAccountList() {
+            if (TempData.TryGetValue("AlertError", out object? error)) ViewBag.AlertError = error;
+            if (TempData.TryGetValue("AlertSuccess", out object? success)) ViewBag.AlertSuccess = success;
 
-        [Authorize(Roles = "1")]
-        public async Task<IActionResult> TypeAccountList()
-        {
-
-
-            
-            return View();
+            return View(await _typeAccountService.GetAllForShow());
         }
 
-
-        public ActionResult showTypeAccount()
-        {
+        public ActionResult CreateTypeAccount() {
             return PartialView("CreateTypeAccount");
         }
 
+        public async Task<IActionResult> UpdateTypeAccount(int? id) {
 
+            if (!id.HasValue) return PartialView("UpdateTypeAccount");
+
+            var typeAccount = await _typeAccountService.GetById((int)id);
+
+            if (typeAccount == null) return PartialView("UpdateTypeAccount");
+
+            return PartialView("UpdateTypeAccount", typeAccount);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateTypeAccountDTO oCreateTypeAccountDTO) {
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+
+            var result = await _typeAccountService.Create(oCreateTypeAccountDTO);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+            script = "<script>AlertSuccess('Tipo de cuenta creada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeAccountList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateTypeAccountDTO oUpdateTypeAccountDTO) {
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+
+            var result = await _typeAccountService.Update(oUpdateTypeAccountDTO);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+            script = "<script>AlertSuccess('Tipo de cuenta actualizado exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeAccountList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id) {
+            string script;
+
+            if (!id.HasValue) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+
+            var result = await _typeAccountService.Remove((int)id);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar el tipo de cuenta, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeAccountList");
+            }
+            script = "<script>AlertSuccess('Tipo de cuenta eliminada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeAccountList");
+        }
     }
 }
-
-
