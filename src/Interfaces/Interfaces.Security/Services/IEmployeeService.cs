7caeb6c0f30d401e20dfb42e4562bca8b108b051
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 02:19:59 -0600

    Added method: GetAllForUser

--- src/Interfaces/Interfaces.Security/Services/IEmployeeService.cs
+++ src/Interfaces/Interfaces.Security/Services/IEmployeeService.cs
@@ -1,8 +1,10 @@
-﻿using Dtos.AlphaBank.Security;
+﻿using Data.AlphaBank;
+using Dtos.AlphaBank.Security;
 
 namespace Interfaces.Security.Services {
     public interface IEmployeeService {
         public Task<List<ShowEmployeeDTO>> GetAll();
+        public Task<List<Employee>> GetAllForUser();
         public Task<bool> Create(CreateEmployeeDTO oCreateEmployeeDTO);
     }
 }
