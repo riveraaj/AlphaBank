a767ef0162cc67e032e03770a526434af30c7ad3
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 13:03:24 -0600

    All the logic for creating, deleting and updating the model has been created.

--- src/Clients/WebClient/Controllers/TypeLoanController.cs
+++ src/Clients/WebClient/Controllers/TypeLoanController.cs
@@ -1,36 +1,111 @@
-﻿using Dtos.AlphaBank.Security;
-using Interfaces.Security.Services;
-using Interfaces.BankAccounts.Services;
+﻿using Dtos.AlphaBank.AnalyzeLoanOpportunities;
+using Interfaces.AnalyzeLoanOpportunities.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using WebClient.Services;
-using WebClient.Models;
-using Data.AlphaBank;
-using Service.BankAccounts;
 
-namespace WebClient.Controllers
+namespace WebClient.Controllers {
+    [Authorize]
+    public class TypeLoanController(ITypeLoanService typeLoanService) : Controller {
 
-{
-    public class TypeLoanController() : Controller
-    {
+        private readonly ITypeLoanService _typeLoanService = typeLoanService;
 
-        
+        public async Task<IActionResult> TypeLoanList() {
+            if (TempData.TryGetValue("AlertError", out object? error)) ViewBag.AlertError = error;
+            if (TempData.TryGetValue("AlertSuccess", out object? success)) ViewBag.AlertSuccess = success;
 
-        [Authorize(Roles = "1")]
-        public async Task<IActionResult> TypeLoanList()
-        {
-
-            return View();
+            return View(await _typeLoanService.GetAllForShow());
         }
 
+        public ActionResult CreateTypeLoan()
+            => PartialView("CreateTypeLoan");
 
-        public ActionResult showTypeLoan()
-        {
-            return PartialView("CreateTypeAccount");
+        public async Task<IActionResult> UpdateTypeLoan(int? id) {
+
+            if (!id.HasValue) return PartialView("UpdateTypeLoan");
+
+            var typeLoan = await _typeLoanService.GetById((int)id);
+
+            if (typeLoan == null) return PartialView("UpdateTypeLoan");
+
+            return PartialView("UpdateTypeLoan", typeLoan);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateTypeLoanDTO oCreateTypeLoanDTO) {
+            string script;
 
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear el tipo de prestamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+
+            var result = await _typeLoanService.Create(oCreateTypeLoanDTO);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido crear el tipo de préstamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+            script = "<script>AlertSuccess('Tipo de préstamo creada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeLoanList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateTypeLoanDTO oUpdateTypeLoanDTO) {
+            string script;
+
+            if (!ModelState.IsValid) {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el tipo de préstamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+
+            var result = await _typeLoanService.Update(oUpdateTypeLoanDTO);
+
+            if (!result)  {
+                script = "<script>AlertError('Hubo un error','No se ha podido actualizar el tipo de préstamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+            script = "<script>AlertSuccess('Tipo de préstamo actualizado exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeLoanList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id) {
+            string script;
+
+            if (!id.HasValue) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar el tipo de préstamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+
+            var result = await _typeLoanService.Remove((int)id);
+
+            if (!result) {
+                script = "<script>AlertError('Hubo un error','No se ha podido eliminar el tipo de préstamo, inténtelo más tarde.');</script>";
+
+                TempData["AlertError"] = script;
+                return RedirectToAction("TypeLoanList");
+            }
+            script = "<script>AlertSuccess('Tipo de préstamo eliminada exitosamente','');</script>";
+
+            TempData["AlertSuccess"] = script;
+            return RedirectToAction("TypeLoanList");
+        }
     }
 }
-
-
