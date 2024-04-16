db7de0b5b3d36a15fa531b69d7826c9b47c49622
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 02:20:34 -0600

    Added method: MapShowUserDTO &  MapUpdateUser

--- src/Mappers/Mapper.Security/UserMapper.cs
+++ src/Mappers/Mapper.Security/UserMapper.cs
@@ -4,6 +4,19 @@ using Dtos.AlphaBank.Security;
 namespace Mapper.Security {
     public static class UserMapper {
 
+        public static ShowUserDTO MapShowUserDTO(User oUser)
+            => new() {
+                Id = oUser.Id.ToString(),
+                EmailAddress = oUser.EmailAddress,
+                Role = oUser.Role.Description,
+                Person = new() {
+                    FirstName = oUser.Employee.Person.FirstName,
+                    Name = oUser.Employee.Person.Name,
+                    PhoneNumber = oUser.Employee.Person.Phones.FirstOrDefault()!.Number,
+                    SecondName = oUser.Employee.Person.SecondName
+                }
+            };
+
         public static User MapUser(CreateUserDTO oCreateUserDTO)
             => new() {
                 EmailAddress = oCreateUserDTO.Email,
@@ -20,5 +33,13 @@ namespace Mapper.Security {
                 Password = oUpdateUserDTO.Password,
                 RoleId = (byte) oUpdateUserDTO.RoleId!
             };
+
+        public static UpdateUserDTO MapUpdateUser(User oUser)
+           => new() {
+               Id = oUser.Id,
+               Email = oUser.EmailAddress,
+               Password = oUser.Password,
+               RoleId = oUser.RoleId!
+           };
     }
 }
