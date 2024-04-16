ac8fb9db1ab6a38fa337f7fd2cf5febd4b1f5408
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 13:05:55 -0600

    The DTO was created to display all catalogs.

---
+++ src/Dtos/Dtos.AlphaBank/AnalyzeLoanOpportunities/ShowCatalogsDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Dtos.AlphaBank.AnalyzeLoanOpportunities {
+    public class ShowCatalogsDTO {
+        public string Id { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
