214e7b6b4ed3a2d2f7775285f7c5bac47f338989
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 02:19:31 -0600

    New properties added

--- src/Dtos/Dtos.AlphaBank/Security/ShowUserDto.cs
+++ src/Dtos/Dtos.AlphaBank/Security/ShowUserDto.cs
@@ -1,6 +1,10 @@
-﻿namespace Dtos.AlphaBank.Security {
+﻿using Dtos.AlphaBank.Common;
+
+namespace Dtos.AlphaBank.Security {
     public class ShowUserDTO {
+        public string Id { get; set; } = null!;
         public string EmailAddress { get; set; } = null!;
         public string Role { get; set; } = null!;
+        public ShowPersonDTO Person { get; set; } = null!;
     }
 }
