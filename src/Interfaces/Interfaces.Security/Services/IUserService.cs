15548a8eaa618932178683051686a949b6a932eb
riveraaj <jonathandavidr7@gmail.com>
2024-04-16 02:20:18 -0600

    Added method: GetById & GetAll

--- src/Interfaces/Interfaces.Security/Services/IUserService.cs
+++ src/Interfaces/Interfaces.Security/Services/IUserService.cs
@@ -1,9 +1,9 @@
 ﻿using Dtos.AlphaBank.Security;
-using Interfaces.Security.Repositories;
 
 namespace Interfaces.Security.Services {
     public interface IUserService {
-        public Task<bool> UserSetup(CreateEmployeeDTO oCreateEmployeeDTO, IEmployeeRepository oEmployeeRepository);
+        public Task<List<ShowUserDTO>> GetAll();
+        public Task<UpdateUserDTO?> GetById(int id); 
         public Task<bool> Create(CreateUserDTO oCreateUserDTO);
         public Task<bool> Update(UpdateUserDTO oUpdateUserDTO);
         public Task<bool> Remove(int id);
