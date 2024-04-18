52380bb0883bb6c3d1456339fe5f738f11e46827
Steven Rodriguez S <stevenrs019@gmail.com>
2024-04-18 01:18:37 -0600

    Reporte Historial de prestamos rechazados

---
+++ src/Clients/WebClient/Controllers/R_HistoryRejectedLoansController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class R_HistoryRejectedLoansController : Controller
+    {
+        public IActionResult HistoryRejectedLoansList()
+        {
+            return View();
+        }
+    }
+}
