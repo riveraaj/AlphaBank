f6efaa911f2a20ff347b8b55e63eefafda416c27
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-18 08:33:16 +0200

    Se creo un DTO para el reporte de Clientes Morosos

---
+++ src/Dtos/Dtos.AlphaBank/ContinueLoans/ShowLoanDefaultersDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Dtos.AlphaBank.ContinueLoans
+{
+    public class ShowLoanDefaultersDTO
+    {
+        public int LoanId { get; set; }
+        public string TypeLoanDescription { get; set; } = null!;
+        public DateOnly DateApplication { get; set; }
+        public int PersonId { get; set; }
+        public decimal Amount { get; set; }
+        public string DaysLate { get; set; } = null!;
+    }
+}
