0fc3b9013b580db335cd2bbc91d91cf8c3f3d0b5
Steven Rodriguez S <stevenrs019@gmail.com>
2024-04-18 01:25:17 -0600

    Historial Préstamos Aprobados

---
+++ src/Clients/WebClient/Controllers/R_HistoryApprovedLoansController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class R_HistoryApprovedLoansController : Controller
+    {
+        public IActionResult HistoryApprovedLoansList()
+        {
+            return View();
+        }
+    }
+}
