aa207dc53a554b4fea091955be16aae8d8e3b58b
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-04-18 09:08:17 +0200

    Agregado el metodo de Reportes de Clientes Morosos a la interfaz del servicio de CollectionHistory

--- src/Interfaces/Repository.ContinueLoans/Services/ICollectionHistoryService.cs
+++ src/Interfaces/Repository.ContinueLoans/Services/ICollectionHistoryService.cs
@@ -5,5 +5,6 @@ namespace Interfaces.ContinueLoans.Services {
     public interface ICollectionHistoryService {
         public Task<bool> Create(CreateCollectionHistoryDTO oCreateCollectionHistoryDTO);
         public Task<List<CollectionHistory>> GetAll();
+        public  Task<List<ShowLoanDefaultersDTO>> GetAllForDefaultersReport();
     }
 }
