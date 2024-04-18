4c93cc74ce712b39d95176bcc9955b91e6004c25
Steven Rodriguez S <stevenrs019@gmail.com>
2024-04-18 01:31:36 -0600

    Reporte Cuentas Cerradas

---
+++ src/Clients/WebClient/Controllers/R_AccountsClosedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class R_AccountsClosedController : Controller
+    {
+        public IActionResult AccountsClosedList()
+        {
+            return View();
+        }
+    }
+}
